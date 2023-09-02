81e29ea620f9c5d9de4034523ec2d81a5ab3720d
George Norfolk <georgenorfolk@hotmail.co.uk>
2023-09-02 22:14:16 +0100

    Adding cron running every minute triggering consumer job.

--- nodejs/tf/consumer.tf
+++ nodejs/tf/consumer.tf
@@ -48,3 +48,22 @@ resource "aws_security_group" "consumer" {
         Name = "${var.app_name}-consumer-lambda-sg"
     }
 }
+
+resource "aws_cloudwatch_event_rule" "consumer" {
+    name = "schedule-consumer-rule"
+    schedule_expression = "cron(*/1 * ? * * *)"
+}
+
+resource "aws_cloudwatch_event_target" "consumer" {
+    rule = aws_cloudwatch_event_rule.consumer.name
+    target_id = "schedule-consumer-target"
+    arn = aws_lambda_function.consumer.arn
+}
+
+resource "aws_lambda_permission" "consumer" {
+    statement_id = "AllowConsumerExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.consumer.function_name
+    principal = "events.amazonaws.com"
+    source_arn = aws_cloudwatch_event_rule.consumer.arn
+}
