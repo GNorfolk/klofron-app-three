25757bc27ad25bfe2ee5b29f38e4ec6212655409
George Norfolk <georgenorfolk@hotmail.co.uk>
2023-09-02 22:34:56 +0100

    Adding promise approach for increase-wood, increase-storage, increase-rooms, create-house, and changing action time back to 8 hours.

--- nodejs/consumer.js
+++ nodejs/consumer.js
@@ -10,7 +10,7 @@ config = {
 
 function checkQueue(connection) {
     new Promise((resolve1, reject1) => {
-        const query1 = 'SELECT id, person_id, type_id, started_at, completed_at, cancelled_at FROM action WHERE started_at IS NOT NULL AND completed_at IS NULL AND cancelled_at IS NULL AND started_at + INTERVAL 1 SECOND < now();'
+        const query1 = 'SELECT id, person_id, type_id, started_at, completed_at, cancelled_at FROM action WHERE started_at IS NOT NULL AND completed_at IS NULL AND cancelled_at IS NULL AND started_at + INTERVAL 8 HOUR < now();'
         connection.query(query1, function(err1, rows1) {
             if (err1) {
                 return reject1(err1)
@@ -43,6 +43,182 @@ function checkQueue(connection) {
                                                 if (row2.storage >= row2.food + row2.wood + 2) {
                                                     return new Promise((resolve3, reject3) => {
                                                         const query3 = 'UPDATE action SET completed_at = NOW() WHERE id = ' + row1['id'] + '; UPDATE house SET food = food + 2 WHERE id = (SELECT house_id FROM person WHERE id = ' + row1['person_id'] + ');'
+                                                        connection.query(query3, function(err3, res3) {
+                                                            if (err3) {
+                                                                return reject3(err3)
+                                                            } else {
+                                                                for (const msg of res3) {
+                                                                    console.log('Action with ID ' + row1['id'] + ' message: ' + msg.message)
+                                                                }
+                                                                resolve3(res3)
+                                                            }
+                                                        })
+                                                    })
+                                                } else {
+                                                    return new Promise((resolve3, reject3) => {
+                                                        const query3 = 'UPDATE action SET completed_at = NOW() WHERE id = ' + row1['id']
+                                                        connection.query(query3, function(err3, res3) {
+                                                            if (err3) {
+                                                                return reject3(err3)
+                                                            } else {
+                                                                console.log('Action with ID ' + row1['id'] + ' message: ' + res3.message)
+                                                                resolve3(res3)
+                                                            }
+                                                        })
+                                                    })
+                                                }
+                                            })
+                                        ).then(() => {
+                                            resolve2(rows2)
+                                        })
+                                    }
+                                })
+                            })
+                        } else if (row1['type_id'] == 2) {
+                            return new Promise((resolve2, reject2) => {
+                                const query2 = 'SELECT house.storage, house.food, house.wood FROM person INNER JOIN house ON person.house_id = house.id WHERE person.id = ' + row1['person_id']
+                                connection.query(query2, function(err2, rows2) {
+                                    if (err2) {
+                                        return reject2(err2)
+                                    } else {
+                                        return Promise.all(
+                                            rows2.map(row2 => {
+                                                console.log('Action with ID ' + row1['id'] + ' has house stats: ' + JSON.stringify(row2))
+                                                if (row2.storage >= row2.food + row2.wood + 1) {
+                                                    return new Promise((resolve3, reject3) => {
+                                                        const query3 = 'UPDATE action SET completed_at = NOW() WHERE id = ' + row1['id'] + '; UPDATE house SET wood = wood + 1 WHERE id = (SELECT house_id FROM person WHERE id = ' + row1['person_id'] + ');'
+                                                        connection.query(query3, function(err3, res3) {
+                                                            if (err3) {
+                                                                return reject3(err3)
+                                                            } else {
+                                                                for (const msg of res3) {
+                                                                    console.log('Action with ID ' + row1['id'] + ' message: ' + msg.message)
+                                                                }
+                                                                resolve3(res3)
+                                                            }
+                                                        })
+                                                    })
+                                                } else {
+                                                    return new Promise((resolve3, reject3) => {
+                                                        const query3 = 'UPDATE action SET completed_at = NOW() WHERE id = ' + row1['id']
+                                                        connection.query(query3, function(err3, res3) {
+                                                            if (err3) {
+                                                                return reject3(err3)
+                                                            } else {
+                                                                console.log('Action with ID ' + row1['id'] + ' message: ' + res3.message)
+                                                                resolve3(res3)
+                                                            }
+                                                        })
+                                                    })
+                                                }
+                                            })
+                                        ).then(() => {
+                                            resolve2(rows2)
+                                        })
+                                    }
+                                })
+                            })
+                        } else if (row1['type_id'] == 3) {
+                            return new Promise((resolve2, reject2) => {
+                                const query2 = 'SELECT house.storage FROM person INNER JOIN house ON person.house_id = house.id WHERE person.id = ' + row1['person_id']
+                                connection.query(query2, function(err2, rows2) {
+                                    if (err2) {
+                                        return reject2(err2)
+                                    } else {
+                                        return Promise.all(
+                                            rows2.map(row2 => {
+                                                console.log('Action with ID ' + row1['id'] + ' has house stats: ' + JSON.stringify(row2))
+                                                if (row2.storage >= row2.food + row2.wood + 1) {
+                                                    return new Promise((resolve3, reject3) => {
+                                                        const query3 = 'UPDATE action SET completed_at = NOW() WHERE id = ' + row1['id'] + '; UPDATE house SET storage = storage + 3 WHERE id = (SELECT house_id FROM person WHERE id = ' + row1['person_id'] + ');'
+                                                        connection.query(query3, function(err3, res3) {
+                                                            if (err3) {
+                                                                return reject3(err3)
+                                                            } else {
+                                                                for (const msg of res3) {
+                                                                    console.log('Action with ID ' + row1['id'] + ' message: ' + msg.message)
+                                                                }
+                                                                resolve3(res3)
+                                                            }
+                                                        })
+                                                    })
+                                                } else {
+                                                    return new Promise((resolve3, reject3) => {
+                                                        const query3 = 'UPDATE action SET completed_at = NOW() WHERE id = ' + row1['id']
+                                                        connection.query(query3, function(err3, res3) {
+                                                            if (err3) {
+                                                                return reject3(err3)
+                                                            } else {
+                                                                console.log('Action with ID ' + row1['id'] + ' message: ' + res3.message)
+                                                                resolve3(res3)
+                                                            }
+                                                        })
+                                                    })
+                                                }
+                                            })
+                                        ).then(() => {
+                                            resolve2(rows2)
+                                        })
+                                    }
+                                })
+                            })
+                        } else if (row1['type_id'] == 4) {
+                            return new Promise((resolve2, reject2) => {
+                                const query2 = 'SELECT house.storage FROM person INNER JOIN house ON person.house_id = house.id WHERE person.id = ' + row1['person_id']
+                                connection.query(query2, function(err2, rows2) {
+                                    if (err2) {
+                                        return reject2(err2)
+                                    } else {
+                                        return Promise.all(
+                                            rows2.map(row2 => {
+                                                console.log('Action with ID ' + row1['id'] + ' has house stats: ' + JSON.stringify(row2))
+                                                if (row2.storage >= row2.food + row2.wood + 1) {
+                                                    return new Promise((resolve3, reject3) => {
+                                                        const query3 = 'UPDATE action SET completed_at = NOW() WHERE id = ' + row1['id'] + '; UPDATE house SET rooms = rooms + 1 WHERE id = (SELECT house_id FROM person WHERE id = ' + row1['person_id'] + ');'
+                                                        connection.query(query3, function(err3, res3) {
+                                                            if (err3) {
+                                                                return reject3(err3)
+                                                            } else {
+                                                                for (const msg of res3) {
+                                                                    console.log('Action with ID ' + row1['id'] + ' message: ' + msg.message)
+                                                                }
+                                                                resolve3(res3)
+                                                            }
+                                                        })
+                                                    })
+                                                } else {
+                                                    return new Promise((resolve3, reject3) => {
+                                                        const query3 = 'UPDATE action SET completed_at = NOW() WHERE id = ' + row1['id']
+                                                        connection.query(query3, function(err3, res3) {
+                                                            if (err3) {
+                                                                return reject3(err3)
+                                                            } else {
+                                                                console.log('Action with ID ' + row1['id'] + ' message: ' + res3.message)
+                                                                resolve3(res3)
+                                                            }
+                                                        })
+                                                    })
+                                                }
+                                            })
+                                        ).then(() => {
+                                            resolve2(rows2)
+                                        })
+                                    }
+                                })
+                            })
+                        } else if (row1['type_id'] == 5) {
+                            return new Promise((resolve2, reject2) => {
+                                const query2 = 'SELECT house.storage FROM person INNER JOIN house ON person.house_id = house.id WHERE person.id = ' + row1['person_id']
+                                connection.query(query2, function(err2, rows2) {
+                                    if (err2) {
+                                        return reject2(err2)
+                                    } else {
+                                        return Promise.all(
+                                            rows2.map(row2 => {
+                                                console.log('Action with ID ' + row1['id'] + ' has house stats: ' + JSON.stringify(row2))
+                                                if (row2.storage >= row2.food + row2.wood + 1) {
+                                                    return new Promise((resolve3, reject3) => {
+                                                        const query3 = 'UPDATE action SET completed_at = NOW() WHERE id = ' + row1['id'] + '; INSERT INTO house (name, rooms, storage, family_id) VALUES (\'House\', 1, 6, (SELECT family_id FROM person WHERE id = ' + row1['person_id'] + ');'
                                                         connection.query(query3, function(err3, res3) {
                                                             if (err3) {
                                                                 return reject3(err3)
