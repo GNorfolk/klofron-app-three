58ce73bd3ea05ef62aea0928e87668d383a7a251
George Norfolk <georgenorfolk@hotmail.co.uk>
2023-09-09 20:50:51 +0100

    Sign in syntax fix.

--- next/pages/auth/signin.tsx
+++ next/pages/auth/signin.tsx
@@ -1,45 +1,47 @@
-import { NextPage } from 'next'
-import { FormEventHandler, useState } from 'react'
-import { signIn } from "next-auth/react"
+import { NextPage } from "next";
+import { signIn } from "next-auth/react";
+import { FormEventHandler, useState } from "react";
 
 interface Props {}
 
-const SignIn: NextPage = (props) : JSX.Element => {
-    const [userInfo, setUserInfo] = useState({ email: "", password: "" })
-    const handleSubmit:FormEventHandler<HTMLFormElement> = (e) => {
-        // validate user info
-        e.preventDefault()
-        const res = signIn('credentals', {
+const SignIn: NextPage = (props): JSX.Element => {
+    const [userInfo, setUserInfo] = useState({ email: "", password: "" });
+    const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+        // validate your userinfo
+        e.preventDefault();
+
+        const res = await signIn("credentials", {
             email: userInfo.email,
             password: userInfo.password,
             redirect: false,
-        })
-        console.log(res)
-    }
+        });
+
+        console.log(res);
+    };
     return (
-        <div>
+        <div className="sign-in-form">
             <form onSubmit={handleSubmit}>
-                <h1>Form</h1>
+                <h1>Login</h1>
                 <input
                     value={userInfo.email}
-                    onChange={({ target }) => 
+                    onChange={({ target }) =>
                         setUserInfo({ ...userInfo, email: target.value })
                     }
                     type="email"
-                    placeholder="example@example.com"
+                    placeholder="john@email.com"
                 />
                 <input
                     value={userInfo.password}
-                    onChange={({ target }) => 
+                    onChange={({ target }) =>
                         setUserInfo({ ...userInfo, password: target.value })
                     }
                     type="password"
                     placeholder="********"
                 />
-                <input type="submit" value="Login" /> 
+                <input type="submit" value="Login" />
             </form>
         </div>
-    )
-}
+    );
+};
 
 export default SignIn;
