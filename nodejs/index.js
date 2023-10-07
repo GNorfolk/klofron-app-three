6e9ff2ed11feb1b0d0e6441ff49315de4211c540
George Norfolk <georgenorfolk@hotmail.co.uk>
2023-10-07 22:51:17 +0100

    Updating post requests to use the resources table rather than house table food and wood columns.

--- nodejs/index.js
+++ nodejs/index.js
@@ -350,14 +350,14 @@ app.post('/v1/increase-food/:id', function(req, res) {
 })
 
 app.post('/v1/decrease-food/:id', function(req, res) {
-    connection.query('SELECT food FROM house WHERE id = ' + req.params.id, function (err, rows) {
+    connection.query("SELECT COALESCE((SELECT volume FROM resource WHERE type_name = 'food' AND house_id = " + req.params.id + "), 0) AS food", function (err, rows) {
         if (err) {
             console.log("DecreaseFoodError: ", err)
             connection = require('./database.js')
             res.json({error: err})
         } else {
             if (rows[0].food > 0) {
-                connection.query('UPDATE house SET food = food - 1 WHERE id = ' + req.params.id, function (err, rows) {
+                connection.query("UPDATE resource SET volume = volume - 1 WHERE type_name = 'food' AND house_id = " + req.params.id, function (err, rows) {
                     if(err) throw err
                 })
                 res.send({"success": true})
@@ -371,7 +371,14 @@ app.post('/v1/decrease-food/:id', function(req, res) {
 })
 
 app.post('/v1/increase-wood/:id', function(req, res) {
-    connection.query('SELECT house.food, (SELECT count(id) FROM action WHERE person_id = ' + req.params.id + ' AND started_at IS NOT NULL AND completed_at IS NULL AND cancelled_at IS NULL) AS count FROM person join house ON person.house_id = house.id WHERE person.id = ' + req.params.id, function (err, rows) {
+    const selectQuery = `
+        SELECT
+            COALESCE((SELECT volume FROM resource WHERE type_name = 'food' AND house_id = house.id), 0) AS food,
+            (SELECT count(id) FROM action WHERE person_id = ` + req.params.id + ` AND started_at IS NOT NULL AND completed_at IS NULL AND cancelled_at IS NULL) AS count
+        FROM person
+            JOIN house ON person.house_id = house.id
+        WHERE person.id = ` + req.params.id
+    connection.query(selectQuery, function (err, rows) {
         if (err) {
             console.log("IncreaseWoodError: ", err)
             connection = require('./database.js')
@@ -379,7 +386,7 @@ app.post('/v1/increase-wood/:id', function(req, res) {
         } else {
             if (rows[0].food >= 1 && rows[0].count == 0) {
                 const infinite = req.query.infinite | 0
-                connection.query('INSERT INTO action (person_id, type_id, started_at, infinite) VALUES (' + req.params.id + ', 2, NOW(), ' + infinite + '); UPDATE house SET food = food - 1 WHERE id = (SELECT house_id FROM person WHERE id = ' + req.params.id + ');', function(err, result) {
+                connection.query("INSERT INTO action (person_id, type_id, started_at, infinite) VALUES (" + req.params.id + ", 2, NOW(), " + infinite + "); UPDATE resource SET volume = volume - 1 WHERE type_name = 'food' AND house_id = " + req.params.id, function(err, result) {
                     if(err) throw err
                 })
                 res.send({"success": true})
@@ -395,14 +402,14 @@ app.post('/v1/increase-wood/:id', function(req, res) {
 })
 
 app.post('/v1/decrease-wood/:id', function(req, res) {
-    connection.query('SELECT wood FROM house WHERE id = ' + req.params.id, function (err, rows) {
+    connection.query("SELECT COALESCE((SELECT volume FROM resource WHERE type_name = 'wood' AND house_id = " + req.params.id + "), 0) AS wood", function (err, rows) {
         if (err) {
             console.log("DecreaseWoodError: ", err)
             connection = require('./database.js')
             res.json({error: err})
         } else {
             if (rows[0].wood > 0) {
-                connection.query('UPDATE house SET wood = wood - 1 WHERE id = ' + req.params.id, function (err, rows) {
+                connection.query("UPDATE resource SET volume = volume - 1 WHERE type_name = 'wood' AND house_id = " + req.params.id, function (err, rows) {
                     if(err) throw err
                 })
                 res.send({"success": true})
@@ -416,7 +423,16 @@ app.post('/v1/decrease-wood/:id', function(req, res) {
 })
 
 app.post('/v1/modify-house/increase-storage/:id', function(req, res) {
-    connection.query('SELECT (SELECT count(id) FROM action WHERE person_id = ' + req.params.id + ' AND started_at IS NOT NULL AND completed_at IS NULL AND cancelled_at IS NULL) AS count, house.storage, house.food, house.wood FROM person join house ON person.house_id = house.id WHERE person.id = ' + req.params.id, function (err, rows) {
+    const selectQuery = `
+        SELECT
+            house.storage, house.id,
+            (SELECT count(id) FROM action WHERE person_id = ` + req.params.id + ` AND started_at IS NOT NULL AND completed_at IS NULL AND cancelled_at IS NULL) AS count,
+            COALESCE((SELECT volume FROM resource WHERE type_name = 'food' AND house_id = house.id), 0) AS food,
+            COALESCE((SELECT volume FROM resource WHERE type_name = 'wood' AND house_id = house.id), 0) AS wood
+        FROM person
+            JOIN house ON person.house_id = house.id
+        WHERE person.id = ` + req.params.id
+    connection.query(selectQuery, function (err, rows) {
         if (err) {
             console.log("IncreaseStorageError: ", err)
             connection = require('./database.js')
@@ -424,7 +440,11 @@ app.post('/v1/modify-house/increase-storage/:id', function(req, res) {
         } else {
             if (rows[0].count == 0 && rows[0].food >= 1 && rows[0].wood >= 3) {
                 const infinite = req.query.infinite | 0
-                connection.query('INSERT INTO action (person_id, type_id, started_at, infinite) VALUES (' + req.params.id + ', 3, NOW(), ' + infinite + '); UPDATE house SET wood = wood - 3, food = food - 1 WHERE id = (SELECT house_id FROM person WHERE id = ' + req.params.id + ');', function(err, result) {
+                insertQuery = `
+                    INSERT INTO action (person_id, type_id, started_at, infinite) VALUES (` + req.params.id + `, 3, NOW(), ` + infinite + `);
+                    UPDATE resource SET volume = volume - 3 WHERE type_name = 'wood' AND house_id = ` + rows[0].id + `;
+                    UPDATE resource SET volume = volume - 1 WHERE type_name = 'food' AND house_id = ` + rows[0].id
+                connection.query(insertQuery, function(err, result) {
                     if(err) throw err
                 })
                 res.send({"success": true})
@@ -442,7 +462,16 @@ app.post('/v1/modify-house/increase-storage/:id', function(req, res) {
 })
 
 app.post('/v1/modify-house/increase-rooms/:id', function(req, res) {
-    connection.query('SELECT (SELECT count(id) FROM action WHERE person_id = ' + req.params.id + ' AND started_at IS NOT NULL AND completed_at IS NULL AND cancelled_at IS NULL) AS count, house.rooms, house.food, house.wood FROM person join house ON person.house_id = house.id WHERE person.id = ' + req.params.id, function (err, rows) {
+    selectQuery = `
+        SELECT
+            house.rooms, house.id,
+            (SELECT count(id) FROM action WHERE person_id = ` + req.params.id + ` AND started_at IS NOT NULL AND completed_at IS NULL AND cancelled_at IS NULL) AS count,
+            COALESCE((SELECT volume FROM resource WHERE type_name = 'food' AND house_id = house.id), 0) AS food,
+            COALESCE((SELECT volume FROM resource WHERE type_name = 'wood' AND house_id = house.id), 0) AS wood
+        FROM person
+            JOIN house ON person.house_id = house.id
+        WHERE person.id = ` + req.params.id
+    connection.query(selectQuery, function (err, rows) {
         if (err) {
             console.log("IncreaseRoomsError: ", err)
             connection = require('./database.js')
@@ -450,7 +479,11 @@ app.post('/v1/modify-house/increase-rooms/:id', function(req, res) {
         } else {
             if (rows[0].count == 0 && rows[0].food >= 1 && rows[0].wood >= 6) {
                 const infinite = req.query.infinite | 0
-                connection.query('INSERT INTO action (person_id, type_id, started_at, infinite) VALUES (' + req.params.id + ', 4, NOW(), ' + infinite + '); UPDATE house SET wood = wood - 6, food = food - 1 WHERE id = (SELECT house_id FROM person WHERE id = ' + req.params.id + ');', function(err, result) {
+                const insertQuery = `
+                    INSERT INTO action (person_id, type_id, started_at, infinite) VALUES (` + req.params.id + `, 4, NOW(), ` + infinite + `);
+                    UPDATE resource SET volume = volume - 6 WHERE type_name = 'wood' AND house_id = ` + rows[0].id + `;
+                    UPDATE resource SET volume = volume - 1 WHERE type_name = 'food' AND house_id = ` + rows[0].id
+                connection.query(insertQuery, function(err, result) {
                     if(err) throw err
                 })
                 res.send({"success": true})
@@ -468,7 +501,21 @@ app.post('/v1/modify-house/increase-rooms/:id', function(req, res) {
 })
 
 app.post('/v1/create-person/:id', function(req, res) {
-    connection.query('SELECT person.id, person.partner_id, person.gender, person.family_id, person.house_id, person.created_at, house.rooms, house.food, (SELECT COUNT(*) FROM person WHERE house_id = ' + req.params.id + ') AS people, (SELECT count(id) FROM action WHERE person_id = person.id AND started_at IS NOT NULL AND completed_at IS NULL AND cancelled_at IS NULL) AS action_count FROM person INNER JOIN house ON person.house_id = house.id WHERE house_id = ' + req.params.id + ' AND father_id NOT IN (SELECT id FROM person WHERE house_id = ' + req.params.id + ') AND mother_id NOT IN (SELECT id FROM person WHERE house_id = ' + req.params.id + ') AND person.partner_id IS NOT NULL ORDER BY gender DESC;', function (err, rows) {
+    selectQuery = `
+        SELECT
+            person.id, person.partner_id, person.gender, person.family_id, person.house_id, person.created_at, house.rooms,
+            COALESCE((SELECT volume FROM resource WHERE type_name = 'food' AND house_id = house.id), 0) AS food,
+            (SELECT COUNT(*) FROM person WHERE house_id = ` + req.params.id + `) AS people,
+            (SELECT count(id) FROM action WHERE person_id = person.id AND started_at IS NOT NULL AND completed_at IS NULL AND cancelled_at IS NULL) AS action_count
+        FROM person
+            INNER JOIN house ON person.house_id = house.id
+        WHERE
+            house_id = ` + req.params.id + `
+            AND father_id NOT IN (SELECT id FROM person WHERE house_id = ` + req.params.id + `)
+            AND mother_id NOT IN (SELECT id FROM person WHERE house_id = ` + req.params.id + `)
+            AND person.partner_id IS NOT NULL
+        ORDER BY gender DESC;`
+    connection.query(selectQuery, function (err, rows) {
         if (err) {
             console.log("CreatePersonError: ", err)
             connection = require('./database.js')
@@ -479,7 +526,17 @@ app.post('/v1/create-person/:id', function(req, res) {
             mother['age'] = Math.floor(((new Date()).valueOf() - (new Date(mother['created_at'])).valueOf()) / day_in_ms)
             const gender = Math.floor(Math.random() * 2) == 0 ? 'male' : 'female'
             if (father.action_count == 0 && mother.action_count == 0 && rows.length == 2 && father.gender == 'male' && mother.gender == 'female' && father.family_id == mother.family_id && father.house_id == mother.house_id && mother.rooms > mother.people && mother.food >= 2 && father.partner_id == mother.id && mother.partner_id == father.id && mother.age < 50) {
-                connection.query("INSERT INTO action (person_id, type_id, started_at) VALUES (" + mother.id + ", 6, NOW()), (" + father.id + ", 6, NOW()); UPDATE house SET food = food - 2 WHERE id = " + mother.house_id + "; INSERT INTO person (name, family_id, father_id, mother_id, gender, house_id) VALUES ('Baby', " + father.family_id + ", " + father.id + ", " + mother.id + ", '" + gender + "', " + father.house_id + ");", function(err, result) {
+                insertQuery = `
+                    INSERT INTO action
+                        (person_id, type_id, started_at)
+                    VALUES
+                        (` + mother.id + `, 6, NOW()), (` + father.id + `, 6, NOW());
+                    UPDATE resource SET volume = volume - 2 WHERE type_name = 'food' AND house_id = ` + mother.house_id + `;
+                    INSERT INTO person
+                        (name, family_id, father_id, mother_id, gender, house_id)
+                    VALUES
+                        ('Baby', ` + father.family_id + `, ` + father.id + `, ` + mother.id + `, '` + gender + `', ` + father.house_id + `);`
+                connection.query(insertQuery, function(err, result) {
                     if(err) throw err
                 })
                 res.send({"success": true})
@@ -517,7 +574,14 @@ app.post('/v1/create-person/:id', function(req, res) {
 })
 
 app.post('/v1/create-house/:id', function(req, res) {
-    connection.query('SELECT wood, food FROM house WHERE id = (SELECT house_id FROM person WHERE id = ' + req.params.id + ');', function (err, rows) {
+    const selectQuery = `
+        SELECT
+            house_id,
+            (SELECT volume FROM resource WHERE type_name = 'food' AND house_id = person.house_id) AS food,
+            (SELECT volume FROM resource WHERE type_name = 'wood' AND house_id = person.house_id) AS wood
+        FROM person
+        WHERE id = ` + req.params.id
+    connection.query(selectQuery, function (err, rows) {
         if (err) {
             console.log("CreateHouseError: ", err)
             connection = require('./database.js')
@@ -525,7 +589,11 @@ app.post('/v1/create-house/:id', function(req, res) {
         } else {
             if (rows[0].wood >= 12 && rows[0].food >= 3) {
                 const infinite = req.query.infinite | 0
-                connection.query("INSERT INTO action (person_id, type_id, started_at, infinite) VALUES (" + req.params.id + ", 5, NOW(), " + infinite + "); UPDATE house SET wood = wood - 12, food = food - 3 WHERE id = (SELECT house_id FROM person WHERE id = " + req.params.id + ");", function(err, result) {
+                const insertQuery = `
+                    INSERT INTO action (person_id, type_id, started_at, infinite) VALUES (` + req.params.id + `, 5, NOW(), ` + infinite + `);
+                    UPDATE resource SET volume = volume - 12 WHERE type_name = 'wood' AND house_id = ` + rows[0].house_id + `;
+                    UPDATE resource SET volume = volume - 3 WHERE type_name = 'food' AND house_id = ` + rows[0].house_id
+                connection.query(insertQuery, function(err, result) {
                     if(err) throw err
                 })
                 res.send({"success": true})
