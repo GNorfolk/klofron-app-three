c4ee0472a45736d835968a5a295f1ee8ab668f67
George Norfolk <georgenorfolk@hotmail.co.uk>
2023-10-18 23:58:43 +0100

    Moving move functionality into consumer so all items to process are run by one script.

--- nodejs/consumer.js
+++ nodejs/consumer.js
@@ -247,19 +247,50 @@ function checkQueue(connection) {
                         }
                     })
                 ).then(() => {
-                    return new Promise((resolveEnd, rejectEnd) => {
-                        connection.end( errEnd => {
-                            if ( errEnd ) {
-                                return rejectEnd(errEnd)
-                            } else {
-                                resolveEnd()
-                            }
-                        })
-                    })
-                }).then(() => {
                     resolve1()
                 })
             }
+        })
+    }).then(() => {
+        return new Promise((resolve1, reject1) => {
+            const query1 = 'SELECT id, person_id, origin_house_id, destination_house_id, started_at, completed_at, cancelled_at FROM move_house WHERE completed_at IS NULL AND cancelled_at IS NULL AND started_at + INTERVAL 8 HOUR < NOW();'
+            connection.query(query1, function(err1, rows1) {
+                if (err1) {
+                    return reject1(err1)
+                } else {
+                    console.log('Number of moves: ' + rows1.length)
+                    return Promise.all(
+                        rows1.map(row1 => {
+                            console.log('Executing move with ID ' + row1['id'])
+                            return new Promise((resolve3, reject3) => {
+                                const query3 = 'UPDATE move_house SET completed_at = NOW() WHERE id = ' + row1['id'] + '; UPDATE person SET house_id = ' + row1['destination_house_id'] + ' WHERE id = ' + row1['person_id']
+                                connection.query(query3, function(err3, res3) {
+                                    if (err3) {
+                                        return reject3(err3)
+                                    } else {
+                                        for (const msg of res3) {
+                                            console.log('Move with ID ' + row1['id'] + ' message: ' + msg.message)
+                                        }
+                                        resolve3(res3)
+                                    }
+                                })
+                            })
+                        })
+                    ).then(() => {
+                        resolve1()
+                    })
+                }
+            })
+        })
+    }).then(() => {
+        return new Promise((resolveEnd, rejectEnd) => {
+            connection.end( errEnd => {
+                if ( errEnd ) {
+                    return rejectEnd(errEnd)
+                } else {
+                    resolveEnd()
+                }
+            })
         })
     })
 }
