c06d88c426ae8e6dd5667455b5fe65b2f7ac9abc
George Norfolk <georgenorfolk@hotmail.co.uk>
2023-12-29 22:30:14 +0000

    Fixing imports for Trade module, adding test script for nest start.

--- nest/src/trade/trade.module.ts
+++ nest/src/trade/trade.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TradeService } from './trade.service';
 import { TradeController } from './trade.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Trade } from './entities/Trade';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Trade])],
   controllers: [TradeController],
   providers: [TradeService],
 })
