b826ddaa1c05033ae13f39ee37bf1cde9decb400
George Norfolk <georgenorfolk@hotmail.co.uk>
2023-12-31 14:40:58 +0000

    Removing DescribeHouse embedded component, moving functionality to DescribeHouseV2.

--- next/components/DescribeHouseV2.tsx
+++ next/components/DescribeHouseV2.tsx
@@ -1,6 +1,6 @@
 import styles from '../styles/main.module.css'
 import { useRouter } from 'next/router'
-import { QueryClientProvider, useQuery } from '@tanstack/react-query'
+import { QueryClientProvider, useQuery, useMutation } from '@tanstack/react-query'
 import ListHousePeople from './ListHousePeople'
 import ListHouseTrades from './ListHouseTrades'
 import ListHouseResources from './ListHouseResources'
@@ -16,6 +16,12 @@ export default function DescribeHouseV2({ queryClient }) {
         fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/house/' + router.query.id).then(
           (res) => res.json(),
         ),
+    })
+
+    const createPerson = useMutation({
+      mutationFn: (id) => {
+        return axios.post(process.env.NEXT_PUBLIC_API_HOST + '/v1/create-person/' + id)
+      },
     })
 
     const [houseInfo, sethouseInfo] = useState({ name: "" })
@@ -36,7 +42,20 @@ export default function DescribeHouseV2({ queryClient }) {
         <h1 className={styles.heading2Xl}>{data.name}</h1>
         <p className={styles.listItem}>{data.name} has {data.rooms} rooms and contains {data.people} people, so has room for {data.rooms - data.people} more people.</p>
         <p className={styles.listItem}>{data.name} has {data.food} food and {data.wood} wood in storage, and {data.food_in_trade} food and {data.wood_in_trade} wood in trade. It can hold {data.storage} items so has {data.storage - data.food - data.wood - data.food_in_trade - data.wood_in_trade} space for more items.</p>
+        <button onClick={
+          () => {
+            createPerson.mutate(data.id, { onSettled: (res) => {
+              queryClient.invalidateQueries()
+              if (!res.data.success) {
+                document.getElementById("change-me-two-" + data.id).innerText = res.data.error
+              } else {
+                document.getElementById("change-me-two-" + data.id).innerText = ' '
+              }
+            }})
+          }
+        } >Create Person</button>
         <ListHousePeople queryClient={queryClient} />
+        <small className={styles.lightText} id={'change-me-two-' + data.id}></small>
         <ListHouseTrades />
         <ListHouseResources queryClient={queryClient} />
         <h3 className={styles.headingMd}>Rename House</h3>
--- next/pages/house/[id].tsx
+++ next/pages/house/[id].tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link'
 import styles from '../../styles/main.module.css'
-import { useRouter } from 'next/router'
-import { QueryClient, QueryClientProvider, useQuery, useMutation } from '@tanstack/react-query'
-import axios from 'axios'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Layout from '../../components/Layout'
 import DescribeHouseV2 from '../../components/DescribeHouseV2'
 
@@ -13,66 +11,10 @@ export default function House() {
     <Layout>
       <QueryClientProvider client={queryClient}>
         <DescribeHouseV2 queryClient={queryClient} />
-        <DescribeHouse />
       </QueryClientProvider>
       <div className={styles.backToHome}>
         <Link href="/">← Back to home</Link>
       </div>
     </Layout>
   )
-}
-
-function DescribeHouse() {
-  const router = useRouter()
-  if (router.isReady) {
-    const { isLoading, error, data } = useQuery({
-      queryKey: ['houseData' + router.query.id],
-      queryFn: () =>
-        fetch(process.env.NEXT_PUBLIC_API_HOST + '/v1/describe-house/' + router.query.id).then(
-          (res) => res.json(),
-        ),
-    })
-
-    const createPerson = useMutation({
-      mutationFn: (id) => {
-        return axios.post(process.env.NEXT_PUBLIC_API_HOST + '/v1/create-person/' + id)
-      },
-    })
-
-    if (isLoading) return <div>Loading...</div>
-    if (error) return <div>Failed to load</div>
-
-    if (data.success) {
-      return (
-        <div>
-          <ul className={styles.list}>
-            {data.data.map(({ id }) => (
-              <li className={styles.listItem} key={id}>
-                <button onClick={
-                  () => {
-                    createPerson.mutate(id, { onSettled: (res) => {
-                      queryClient.invalidateQueries()
-                      if (!res.data.success) {
-                        document.getElementById("change-me-two-" + id).innerText = res.data.error
-                      } else {
-                        document.getElementById("change-me-two-" + id).innerText = ' '
-                      }
-                    }})
-                  }
-                } >Create Person</button>
-                <small className={styles.lightText} id={'change-me-two-' + id}></small>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          <h2 className={styles.headingLg}>House Info</h2>
-          <p>Backend call failed with error: {data.error}</p>
-        </div>
-      )
-    }
-  }
 }
