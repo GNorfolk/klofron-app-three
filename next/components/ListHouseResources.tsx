831ede1fdb64e880474486f522e06edccfba71c4
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-01-03 19:46:53 +0000

    Removing /v1/describe-house-resources usage on frontend.

--- next/components/ListHouseResources.tsx
+++ next/components/ListHouseResources.tsx
@@ -3,13 +3,13 @@ import { useRouter } from 'next/router'
 import { useQuery, useMutation } from '@tanstack/react-query'
 import axios from 'axios'
 
-export default function ListHouseTrades({ queryClient }) {
+export default function ListHouseResources({ queryClient }) {
   const router = useRouter()
   if (router.isReady) {
     const { isLoading, error, data } = useQuery({
       queryKey: ['resourceData' + router.query.id],
       queryFn: () =>
-        fetch(process.env.NEXT_PUBLIC_API_HOST + '/v1/describe-house-resources/' + router.query.id).then(
+        fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/house/' + router.query.id).then(
           (res) => res.json(),
         ),
     })
@@ -29,42 +29,33 @@ export default function ListHouseTrades({ queryClient }) {
     if (isLoading) return <div>Loading...</div>
     if (error) return <div>Failed to load</div>
 
-    if (data.success) {
-      return (
-        <div>
-          <h2 className={styles.headingLg}>Manage Resources</h2>
-          <p>{data.data[0].name} has {data.data[0].food} food and {data.data[0].wood} wood in storage!</p>
-          <ul className={styles.list}>
-            <li className={styles.listItem}>
-              <p>Wood: {data.data[0].wood} in storage!</p>
-              <button onClick={
-                () => {
-                    decreaseWood.mutate(data.data[0].id, { onSettled: (res) => {
-                    queryClient.invalidateQueries()
-                  }})
-                }
-              } >Decrease Wood</button>
-            </li>
-            <li className={styles.listItem}>
-              <p>Food: {data.data[0].food} in storage!</p>
-              <button onClick={
-                () => {
-                    decreaseFood.mutate(data.data[0].id, { onSettled: (res) => {
-                    queryClient.invalidateQueries()
-                  }})
-                }
-              } >Decrease Food</button>
-            </li>
-          </ul>
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          <h2 className={styles.headingLg}>Manage Resources</h2>
-          <p>Backend call failed with error: {data.error}</p>
-        </div>
-      )
-    }
+    return (
+      <div>
+        <h2 className={styles.headingLg}>Manage Resources</h2>
+        <p>{data.house_name} has {data.house_food} food and {data.house_wood} wood in storage!</p>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>
+            <p>Wood: {data.house_wood} in storage!</p>
+            <button onClick={
+              () => {
+                  decreaseWood.mutate(data.house_id, { onSettled: (res) => {
+                  queryClient.invalidateQueries()
+                }})
+              }
+            } >Decrease Wood</button>
+          </li>
+          <li className={styles.listItem}>
+            <p>Food: {data.house_food} in storage!</p>
+            <button onClick={
+              () => {
+                  decreaseFood.mutate(data.house_id, { onSettled: (res) => {
+                  queryClient.invalidateQueries()
+                }})
+              }
+            } >Decrease Food</button>
+          </li>
+        </ul>
+      </div>
+    )
   }
 }
