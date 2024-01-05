7e016e5156cc0f97d55736f5622ca8ba387e03c9
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-01-05 21:51:09 +0000

    Moving house/index function into ListAllHouses component function.

---
+++ next/components/ListAllHouses.tsx
@@ -0,0 +1,28 @@
+import styles from '../styles/main.module.css'
+import { useQuery } from '@tanstack/react-query'
+
+export default function ListAllFamilies() {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['housesData'],
+    queryFn: () =>
+      fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/house').then(
+        (res) => res.json(),
+      ),
+  })
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>Failed to load</div>
+
+  return (
+    <div>
+      <h2 className={styles.headingLg}>Houses</h2>
+      <ul className={styles.list}>
+        {data.map(({ house_id, house_name, house_rooms, house_storage, house_food, house_wood }) => (
+          <li className={styles.listItem} key={house_id}>
+            <p>The {house_name} house has {house_rooms} rooms and {house_storage} storage. It has {house_food.resource_volume} food and {house_wood.resource_volume} wood in storage.</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
--- next/pages/house/index.tsx
+++ next/pages/house/index.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import styles from '../../styles/main.module.css'
 import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
 import Layout from '../../components/Layout'
+import ListAllHouses from '../../components/ListAllHouses'
 
 const queryClient = new QueryClient()
 
@@ -9,37 +10,11 @@ export default function Home() {
   return (
     <Layout>
       <QueryClientProvider client={queryClient}>
-        <ListHouses />
+        <ListAllHouses />
       </QueryClientProvider>
       <div className={styles.backToHome}>
         <Link href="/">← Back to home</Link>
       </div>
     </Layout>
-  )
-}
-
-function ListHouses() {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ['housesData'],
-    queryFn: () =>
-      fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/house').then(
-        (res) => res.json(),
-      ),
-  })
-
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div>Failed to load</div>
-
-  return (
-    <div>
-      <h2 className={styles.headingLg}>Houses</h2>
-      <ul className={styles.list}>
-        {data.map(({ house_id, house_name, house_rooms, house_storage, house_food, house_wood }) => (
-          <li className={styles.listItem} key={house_id}>
-            <p>The {house_name} house has {house_rooms} rooms and {house_storage} storage. It has {house_food.resource_volume} food and {house_wood.resource_volume} wood in storage.</p>
-          </li>
-        ))}
-      </ul>
-    </div>
   )
 }
