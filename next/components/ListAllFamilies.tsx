c24143b61e9320c10191f10276923d30689bd08a
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-01-05 21:48:39 +0000

    Moving family/index function into ListAllFamilies component funciton.

---
+++ next/components/ListAllFamilies.tsx
@@ -0,0 +1,29 @@
+import styles from '../styles/main.module.css'
+import { useQuery } from '@tanstack/react-query'
+import Link from 'next/link'
+
+export default function ListAllFamilies() {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['familiesData'],
+    queryFn: () =>
+      fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/family').then(
+        (res) => res.json(),
+      ),
+  })
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>Failed to load</div>
+
+  return (
+    <div>
+      <h2 className={styles.headingLg}>Families</h2>
+      <ul className={styles.list}>
+        {data.map(({ family_id, family_name }) => (
+          <li className={styles.listItem} key={family_id}>
+            <p>The <Link href={`/family/${family_id}`}>{family_name}</Link> family.</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
--- next/pages/family/index.tsx
+++ next/pages/family/index.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link'
 import styles from '../../styles/main.module.css'
-import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Layout from '../../components/Layout'
+import ListAllFamilies from '../../components/ListAllFamilies'
 
 const queryClient = new QueryClient()
 
@@ -9,37 +10,11 @@ export default function Home() {
   return (
     <Layout>
       <QueryClientProvider client={queryClient}>
-        <ListFamilies />
+        <ListAllFamilies />
       </QueryClientProvider>
       <div className={styles.backToHome}>
         <Link href="/">← Back to home</Link>
       </div>
     </Layout>
-  )
-}
-
-function ListFamilies() {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ['familiesData'],
-    queryFn: () =>
-      fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/family').then(
-        (res) => res.json(),
-      ),
-  })
-
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div>Failed to load</div>
-
-  return (
-    <div>
-      <h2 className={styles.headingLg}>Families</h2>
-      <ul className={styles.list}>
-        {data.map(({ family_id, family_name }) => (
-          <li className={styles.listItem} key={family_id}>
-            <p>The <Link href={`/family/${family_id}`}>{family_name}</Link> family.</p>
-          </li>
-        ))}
-      </ul>
-    </div>
   )
 }
