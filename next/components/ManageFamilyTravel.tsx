10850165d55958db3b5ef654eb183448f354d2de
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-04-24 21:54:30 +0100

    Adding read data to travel page.

--- next/components/ManageFamilyTravel.tsx
+++ next/components/ManageFamilyTravel.tsx
@@ -2,13 +2,13 @@ import styles from '../styles/main.module.css'
 import { useRouter } from 'next/router'
 import { useQuery } from '@tanstack/react-query'
 
-export default function ManageFamilyTravel() {
+export default function ManageFamilyTravel({ familyId}) {
   const router = useRouter()
   if (router.isReady) {
     const { isLoading, error, data } = useQuery({
       queryKey: ['familyTravelData' + router.query.id],
       queryFn: () =>
-        fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/family/' + router.query.id).then(
+        fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/person?family_id=' + familyId).then(
           (res) => res.json(),
         ),
     })
@@ -24,7 +24,9 @@ export default function ManageFamilyTravel() {
     return (
       <div>
         <h2 className={styles.headingLg}>Travel Info</h2>
-        <p>This is a test</p>
+        { data.map(({ person_name, person_house_id, person_house }) => (
+          person_house_id ? <p>{person_name} lives at {person_house.house_name}.</p> : <p>{person_name} is unhoused.</p>
+        ))}
       </div>
     )
   }
