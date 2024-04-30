3c435baf9cc61113b0992eae39a736cad49f0a3c
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-04-30 16:01:05 +0100

    Hiding the family travel subpage if not logged in as the owning user.

--- next/components/DescribeFamily.tsx
+++ next/components/DescribeFamily.tsx
@@ -1,27 +1,41 @@
 import styles from '../styles/main.module.css'
 import { QueryClientProvider, useQuery } from '@tanstack/react-query'
+import { useRouter } from 'next/router'
 import ListFamilyPeople from './ListFamilyPeople'
 import ListFamilyHouses from './ListFamilyHouses'
 import ListFamilyTravel from './ListFamilyTravel'
 
-export default function DescribeFamily({ queryClient, familyId }) {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ['familyData' + familyId],
-    queryFn: () =>
-      fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/family/' + familyId).then(
-        (res) => res.json(),
-      ),
-  })
+export default function DescribeFamily({ queryClient, userId }) {
+  const router = useRouter()
+  if (router.isReady) {
+    const { isLoading, error, data } = useQuery({
+      queryKey: ['familyData' + router.query.id],
+      queryFn: () =>
+        fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/family/' + router.query.id).then(
+          (res) => res.json(),
+        ),
+    })
 
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div>Failed to load</div>
+    if (isLoading) return <div>Loading...</div>
+    if (error) return <div>Failed to load</div>
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <h1 className={styles.heading2Xl} key={data.family_id}>The {data.family_name} family</h1>
-      <ListFamilyPeople queryClient={queryClient} familyId={familyId} />
-      <ListFamilyHouses queryClient={queryClient} familyId={familyId} />
-      <ListFamilyTravel />
-    </QueryClientProvider>
-  )
+    if (data.family_user_id === userId) {
+      return (
+        <QueryClientProvider client={queryClient}>
+          <h1 className={styles.heading2Xl} key={data.family_id}>The {data.family_name} family</h1>
+          <ListFamilyPeople queryClient={queryClient} familyId={router.query.id} />
+          <ListFamilyHouses queryClient={queryClient} familyId={router.query.id} />
+          <ListFamilyTravel />
+        </QueryClientProvider>
+      )
+    } else {
+      return (
+        <QueryClientProvider client={queryClient}>
+          <h1 className={styles.heading2Xl} key={data.family_id}>The {data.family_name} family</h1>
+          <ListFamilyPeople queryClient={queryClient} familyId={router.query.id} />
+          <ListFamilyHouses queryClient={queryClient} familyId={router.query.id} />
+        </QueryClientProvider>
+      )
+    }
+  }
 }
--- next/pages/family/[id].tsx
+++ next/pages/family/[id].tsx
@@ -1,24 +1,23 @@
 import Link from 'next/link'
 import styles from '../../styles/main.module.css'
-import { useRouter } from 'next/router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Layout from '../../components/Layout'
 import DescribeFamily from '../../components/DescribeFamily'
+import { useSession } from 'next-auth/react'
 
 const queryClient = new QueryClient()
 
 export default function Family() {
-  const router = useRouter()
-  if (router.isReady) {
-    return (
-      <Layout>
+  const { status, data } = useSession()
+  const userId = data?.user ? data.user.id : null
+  return (
+    <Layout>
       <QueryClientProvider client={queryClient}>
-        <DescribeFamily queryClient={queryClient} familyId={router.query.id}/>
+        <DescribeFamily queryClient={queryClient} userId={userId} />
       </QueryClientProvider>
       <div className={styles.backToHome}>
         <Link href="/">← Back to home</Link>
       </div>
     </Layout>
-    )
-  }
+  )
 }
