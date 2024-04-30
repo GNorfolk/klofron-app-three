731870757675462de35bec11d3a276e4e0569312
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-04-30 16:19:29 +0100

    Setting up a redirect on ManageFamilyTravel if not logged in family.

--- next/components/ManageFamilyTravel.tsx
+++ next/components/ManageFamilyTravel.tsx
@@ -4,13 +4,13 @@ import { useQuery } from '@tanstack/react-query'
 import { useForm, SubmitHandler } from "react-hook-form"
 import axios from 'axios'
 
-export default function ManageFamilyTravel({ familyId, queryClient }) {
+export default function ManageFamilyTravel({ queryClient, userId }) {
   const router = useRouter()
   if (router.isReady) {
     const { isLoading, error, data } = useQuery({
       queryKey: ['familyTravelData' + router.query.id],
       queryFn: () =>
-        fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/family/' + familyId).then(
+        fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/family/' + router.query.id).then(
           (res) => res.json(),
         ),
     })
@@ -32,9 +32,9 @@ export default function ManageFamilyTravel({ familyId, queryClient }) {
         house_id: formData.house_id
       }).then(response => {
         queryClient.invalidateQueries()
-        document.getElementById("cm-" + familyId).innerText = ' '
+        document.getElementById("cm-" + router.query.id).innerText = ' '
       }).catch(error => {
-        document.getElementById("cm-" + familyId).innerText = error.toString()
+        document.getElementById("cm-" + router.query.id).innerText = error.toString()
       })
     }
 
@@ -46,28 +46,32 @@ export default function ManageFamilyTravel({ familyId, queryClient }) {
     )
     if (error) return <div>Failed to load</div>
 
-    return (
-      <div>
-        <h2 className={styles.headingLg}>Travel Info</h2>
-        { data.family_people.map(({ person_name, person_house_id, person_house }) => (
-          person_house_id ? <p>{person_name} lives at {person_house.house_name}.</p> : <p>{person_name} is unhoused.</p>
-        ))}
-        <h2 className={styles.headingLg}>Manage Travel</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <select {...register("person_id")}>
-          { data.family_people.map(({ person_id, person_name }) => (
-            <option value={person_id}>{person_name}</option>
+    if (data.family_user_id === userId) {
+      return (
+        <div>
+          <h2 className={styles.headingLg}>Travel Info</h2>
+          { data.family_people.map(({ person_name, person_house_id, person_house }) => (
+            person_house_id ? <p>{person_name} lives at {person_house.house_name}.</p> : <p>{person_name} is unhoused.</p>
           ))}
-          </select>
-          <select {...register("house_id")}>
-          { data.family_houses.map(({ house_id, house_name }) => (
-            <option value={house_id}>{house_name}</option>
-          ))}
-          </select>
-          <input type="submit" />
-        </form>
-        <small className={styles.lightText} id={'cm-' + router.query.id}></small>
-      </div>
-    )
+          <h2 className={styles.headingLg}>Manage Travel</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <select {...register("person_id")}>
+            { data.family_people.map(({ person_id, person_name }) => (
+              <option value={person_id}>{person_name}</option>
+            ))}
+            </select>
+            <select {...register("house_id")}>
+            { data.family_houses.map(({ house_id, house_name }) => (
+              <option value={house_id}>{house_name}</option>
+            ))}
+            </select>
+            <input type="submit" />
+          </form>
+          <small className={styles.lightText} id={'cm-' + router.query.id}></small>
+        </div>
+      )
+    } else {
+      router.push('/')
+    }
   }
 }
--- next/pages/family/[id]/travel.tsx
+++ next/pages/family/[id]/travel.tsx
@@ -4,21 +4,21 @@ import { useRouter } from 'next/router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Layout from '../../../components/Layout'
 import ManageFamilyTravel from '../../../components/ManageFamilyTravel'
+import { useSession } from 'next-auth/react'
 
 const queryClient = new QueryClient()
 
 export default function Family() {
-  const router = useRouter()
-  if (router.isReady) {
-    return (
-      <Layout>
-      <QueryClientProvider client={queryClient}>
-        <ManageFamilyTravel queryClient={queryClient} familyId={router.query.id} />
-      </QueryClientProvider>
-      <div className={styles.backToHome}>
-        <Link href="/">← Back to home</Link>
-      </div>
-    </Layout>
-    )
-  }
+  const { status, data } = useSession()
+  const userId = data?.user ? data.user.id : null
+  return (
+    <Layout>
+    <QueryClientProvider client={queryClient}>
+      <ManageFamilyTravel queryClient={queryClient} userId={userId} />
+    </QueryClientProvider>
+    <div className={styles.backToHome}>
+      <Link href="/">← Back to home</Link>
+    </div>
+  </Layout>
+  )
 }
