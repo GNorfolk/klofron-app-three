e9a8605789e1c72fee8ad3ec53134d2eea5e5343
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-05-03 15:34:19 +0100

    Preventing moving of resources if house or person will end up with more resources than capacity.

--- nest/src/resource/resource.service.ts
+++ nest/src/resource/resource.service.ts
@@ -2,6 +2,8 @@ import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Resource } from './entities/Resource';
 import { Repository, DataSource } from 'typeorm';
+import { Person } from '../person/entities/Person';
+import { House } from '../house/entities/House';
 
 @Injectable()
 export class ResourceService {
@@ -37,6 +39,20 @@ export class ResourceService {
         resource_house_id: body.house_id
       }, "resource_volume", body.resource_volume)
       if (inc.affected != 1 || dec.affected != 1) throw [inc, dec]
+      const person = await queryRunner.manager
+        .createQueryBuilder(Person, "person")
+        .leftJoinAndSelect("person.person_food", "person_food", "person_food.type_name = 'food'")
+        .leftJoinAndSelect("person.person_wood", "person_wood", "person_wood.type_name = 'wood'")
+        .where("person.person_id = :id", { id: body.person_id })
+        .getOne()
+      if (person.person_food.resource_volume + person.person_wood.resource_volume > 3) throw person
+      const house = await queryRunner.manager
+        .createQueryBuilder(House, "house")
+        .innerJoinAndSelect("house.house_food", "food", "food.type_name = 'food'")
+        .innerJoinAndSelect("house.house_wood", "wood", "wood.type_name = 'wood'")
+        .where("house.house_id = :id", { id: body.house_id })
+        .getOne()
+      if (house.house_food.resource_volume + house.house_wood.resource_volume > house.house_storage) throw house
       await queryRunner.commitTransaction();
       await queryRunner.release();
       return [inc, dec];
