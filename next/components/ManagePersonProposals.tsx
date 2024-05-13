de9a0dbbb2a03f2ed1762097c5ea7e337e314d79
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-05-13 08:49:33 +0100

    Moving router usage to component, passing userId to component from page.

--- next/components/ManagePersonProposals.tsx
+++ next/components/ManagePersonProposals.tsx
@@ -1,59 +1,65 @@
 import styles from '../styles/main.module.css'
+import { useRouter } from 'next/router'
 import { useQuery, useMutation } from '@tanstack/react-query'
 import Link from 'next/link'
 import axios from 'axios'
 
-export default function ManagePersonProposals({ accepterPersonId = null, queryClient = null }) {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ['listProposalsData'],
-    queryFn: () =>
-      fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/proposal').then(
-        (res) => res.json(),
-      ),
-  })
+export default function ManagePersonProposals({ userId, queryClient = null }) {
+  const router = useRouter()
+  if (router.isReady) {
+    const { isLoading, error, data } = useQuery({
+      queryKey: ['listProposalsData'],
+      queryFn: () =>
+        fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/proposal').then(
+          // ToDo: Do something different with this data set so we can compare userId, mebe get person data and proposal data somehow
+          (res) => res.json(),
+        ),
+    })
 
-  const acceptProposal = useMutation({
-    mutationFn: (proposer_person_id) => {
-      return axios.post(process.env.NEXT_PUBLIC_API_HOST + '/v1/accept-proposal', {
-        proposer_id: proposer_person_id,
-        accepter_id: accepterPersonId
-      })
-    },
-  })
+    const acceptProposal = useMutation({
+      mutationFn: (proposer_person_id) => {
+        return axios.post(process.env.NEXT_PUBLIC_API_HOST + '/v1/accept-proposal', {
+          proposer_id: proposer_person_id,
+          accepter_id: router.query.id
+        })
+      },
+    })
 
-  if (isLoading) return (
-    <div>
-      <h2 className={styles.headingLg}>Proposal Info</h2>
-      <p>Loading...</p>
-    </div>
-  )
-  if (error) return <div>Failed to load</div>
+    if (isLoading) return (
+      <div>
+        <h2 className={styles.headingLg}>Proposal Info</h2>
+        <p>Loading...</p>
+      </div>
+    )
+    if (error) return <div>Failed to load</div>
 
-  const proposals = data.filter(prop => prop.proposal_id != 30)
+    const proposals = data.filter(prop => prop.proposal_id != 30)
 
-  return (
-    <div>
-      <h2 className={styles.headingLg}>Proposal Info</h2>
-      <ul className={styles.list}>
-        {proposals.map(({ proposal_id, proposal_proposer_person_id, proposal_proposer_person }) => (
-          <li className={styles.listItem} key={proposal_id}>
-            { queryClient || accepterPersonId ? 
-            <>
-              <Link href={"/person/" + proposal_proposer_person_id}>{proposal_proposer_person.person_name + " " + proposal_proposer_person.person_family.family_name + ": "}</Link>
-              <button onClick={
-                () => {
-                  acceptProposal.mutate(proposal_proposer_person_id, {
-                    onSettled: (res) => {
-                      queryClient.invalidateQueries()
-                    }
-                  })
-                }
-              }>Accept Proposal</button>
-            </> : <Link href={"/person/" + proposal_proposer_person_id}>{proposal_proposer_person.person_name + " " + proposal_proposer_person.person_family.family_name + "."}</Link>
-            }
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+    return (
+      <div>
+        <h2 className={styles.headingLg}>Proposal Info</h2>
+        <ul className={styles.list}>
+          {proposals.map(({ proposal_id, proposal_proposer_person_id, proposal_proposer_person }) => (
+            <li className={styles.listItem} key={proposal_id}>
+              {/* ToDo: Add something like data.house_family.family_user_id === userId conditional */}
+              { queryClient ? 
+              <>
+                <Link href={"/person/" + proposal_proposer_person_id}>{proposal_proposer_person.person_name + " " + proposal_proposer_person.person_family.family_name + ": "}</Link>
+                <button onClick={
+                  () => {
+                    acceptProposal.mutate(proposal_proposer_person_id, {
+                      onSettled: (res) => {
+                        queryClient.invalidateQueries()
+                      }
+                    })
+                  }
+                }>Accept Proposal</button>
+              </> : <Link href={"/person/" + proposal_proposer_person_id}>{proposal_proposer_person.person_name + " " + proposal_proposer_person.person_family.family_name + "."}</Link>
+              }
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
--- next/pages/person/[id]/proposal.tsx
+++ next/pages/person/[id]/proposal.tsx
@@ -1,24 +1,23 @@
 import Link from 'next/link'
 import styles from '../../../styles/main.module.css'
-import { useRouter } from 'next/router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Layout from '../../../components/Layout'
 import ManagePersonProposals from '../../../components/ManagePersonProposals'
+import { useSession } from 'next-auth/react'
 
 const queryClient = new QueryClient()
 
 export default function Family() {
-  const router = useRouter()
-  if (router.isReady) {
-    return (
-      <Layout>
-        <QueryClientProvider client={queryClient}>
-          <ManagePersonProposals accepterPersonId={router.query.id} queryClient={queryClient} />
-        </QueryClientProvider>
-        <div className={styles.backToHome}>
-          <Link href="/">← Back to home</Link>
-        </div>
-      </Layout>
-    )
-  }
+  const { status, data } = useSession()
+  const userId = data?.user ? data.user.id : null
+  return (
+    <Layout>
+      <QueryClientProvider client={queryClient}>
+        <ManagePersonProposals userId={userId} queryClient={queryClient} />
+      </QueryClientProvider>
+      <div className={styles.backToHome}>
+        <Link href="/">← Back to home</Link>
+      </div>
+    </Layout>
+  )
 }
