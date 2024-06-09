f59360e03b75010bfae4fde06efd7e9b5eb5461e
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-06-09 18:54:10 +0100

    Adding in variable height boxes split down centre screen layout.

--- next/pages/index/index.tsx
+++ next/pages/index/index.tsx
@@ -3,16 +3,19 @@ import { BaseLayout } from '../../@/components/component/base-layout'
 import styles from '../../styles/main.module.css'
 import { QueryClientProvider } from '@tanstack/react-query'
 import ListAllEntities from '../../components/ListAllEntities'
+import { BoxLayout } from '../../@/components/component/box-layout'
 
 export default function Family({ client }) {
   return (
     <BaseLayout>
-      <QueryClientProvider client={client}>
+      {/* <QueryClientProvider client={client}>
         <ListAllEntities queryClient={client} />
+
       </QueryClientProvider>
       <div className={styles.backToHome}>
         <Link href="/">← Back to home</Link>
-      </div>
+      </div> */}
+      <BoxLayout />
     </BaseLayout>
   )
 }
