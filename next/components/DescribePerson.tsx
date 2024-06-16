354c99bf0067dc91f605a1923b072868a53111d0
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-06-16 22:54:44 +0100

    Moving DescribePerson details into RenamePerson and ListPersonInfo functions.

--- next/components/DescribePerson.tsx
+++ next/components/DescribePerson.tsx
@@ -17,55 +17,19 @@ export default function DescribePerson({ queryClient, status, userId = null }) {
         ),
     })
 
-    const [personInfo, setpersonInfo] = useState({ name: "" })
-    const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
-      e.preventDefault();
-      return axios.patch(process.env.NEXT_PUBLIC_API_HOST + '/v2/person/' + router.query.person_id, {
-        name: personInfo.name
-      }).then((value) => {
-        queryClient.invalidateQueries()
-      })
-    };
-
     if (isLoading) return <div>Loading...</div>
     if (error) return <div>Failed to load</div>
 
     return (
       <QueryClientProvider client={queryClient}>
-        <h2 className={styles.headingLg}>Person Info</h2>
-        <ul className={styles.list}>
-          <li className={styles.listItem} key={data.person_id}>
-            {
-              data.person_partner_id ?
-                <p>{data.person_name} {data.person_family_name} is {data.person_gender} and {data.person_age} years old and is married to <Link href={"/person/" + data.person_partner_id}>{data.person_partner.person_name + " " + data.person_partner.person_family.family_name}</Link>.</p>
-              :
-              <p>{data.person_name} {data.person_family_name} is {data.person_gender} and {data.person_age} years old.</p>
-            }
-            <p>{data.person_name}'s father is <Link href={"/person/" + data.person_father.person_id}><a onClick={(e) => queryClient.invalidateQueries()}>{data.person_father.person_name + ' ' + data.person_father.person_family.family_name}</a></Link> and their mother is <Link href={"/person/" + data.person_mother.person_id}><a onClick={(e) => queryClient.invalidateQueries()}>{data.person_mother.person_name + ' ' + data.person_mother.person_family.family_name}</a></Link>.</p>
-            {
-              data.person_house_id ? <p>{data.person_name} lives at <Link href={"/house/" + data.person_house_id}>{data.person_house.house_address.house_address_number + " " + data.person_house.house_address.house_address_road.house_road_name}</Link>.</p> : <p>{data.person_name} is homeless.</p>
-            }
-          </li>
-        </ul>
+        <ListPersonInfo queryClient={queryClient} data={data}/>
         <h2 className={styles.headingLg}>Actions Info</h2>
         <ListPersonActionsCurrent queryClient={queryClient} personId={router.query.person_id} />
         <ListPersonActionsPrevious personId={router.query.person_id} />
         {
           status === "authenticated" && userId == data.person_family.family_user_id ?
           <div>
-            <h2 className={styles.headingLg}>Rename Person</h2>
-            <ul className={styles.list}>
-              <form onSubmit={handleSubmit}>
-                <input
-                  value={personInfo.name}
-                  onChange={({ target }) =>
-                    setpersonInfo({ ...personInfo, name: target.value })
-                  }
-                  placeholder="Insert name here"
-                />
-                <input type="submit" value="Rename" />
-              </form>
-            </ul>
+            <RenamePerson queryClient={queryClient} personId={router.query.person_id} />
             <ListPersonProposals data={data} userId={userId} showOffers={false} />
           </div>
           :
@@ -290,4 +254,56 @@ export function ListPersonProposals({ data, showLink = true, queryClient = null,
       </div>
     )
   }
+}
+
+export function RenamePerson({ queryClient, personId }) {
+  const [personInfo, setpersonInfo] = useState({ name: "" })
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    return axios.patch(process.env.NEXT_PUBLIC_API_HOST + '/v2/person/' + personId, {
+      name: personInfo.name
+    }).then((value) => {
+      queryClient.invalidateQueries()
+    })
+  };
+
+  return (
+    <div>
+      <h2 className={styles.headingLg}>Rename Person</h2>
+      <ul className={styles.list}>
+        <form onSubmit={handleSubmit}>
+          <input
+            value={personInfo.name}
+            onChange={({ target }) =>
+              setpersonInfo({ ...personInfo, name: target.value })
+            }
+            placeholder="Insert name here"
+          />
+          <input type="submit" value="Rename" />
+        </form>
+      </ul>
+    </div>
+  )
+}
+
+export function ListPersonInfo({ data, queryClient }) {
+  return (
+    <div>
+      <h2 className={styles.headingLg}>Person Info</h2>
+      <ul className={styles.list}>
+        <li className={styles.listItem} key={data.person_id}>
+          {
+            data.person_partner_id ?
+              <p>{data.person_name} {data.person_family_name} is {data.person_gender} and {data.person_age} years old and is married to <Link href={"/person/" + data.person_partner_id}>{data.person_partner.person_name + " " + data.person_partner.person_family.family_name}</Link>.</p>
+            :
+            <p>{data.person_name} {data.person_family_name} is {data.person_gender} and {data.person_age} years old.</p>
+          }
+          <p>{data.person_name}'s father is <Link href={"/person/" + data.person_father.person_id}><a onClick={(e) => queryClient.invalidateQueries()}>{data.person_father.person_name + ' ' + data.person_father.person_family.family_name}</a></Link> and their mother is <Link href={"/person/" + data.person_mother.person_id}><a onClick={(e) => queryClient.invalidateQueries()}>{data.person_mother.person_name + ' ' + data.person_mother.person_family.family_name}</a></Link>.</p>
+          {
+            data.person_house_id ? <p>{data.person_name} lives at <Link href={"/house/" + data.person_house_id}>{data.person_house.house_address.house_address_number + " " + data.person_house.house_address.house_address_road.house_road_name}</Link>.</p> : <p>{data.person_name} is homeless.</p>
+          }
+        </li>
+      </ul>
+    </div>
+  )
 }
