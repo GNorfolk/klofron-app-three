6329acf9c259c71543230c09295c8963b88b6e43
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-07-03 10:50:50 +0100

    Adding container layout and using listing component for /person and /house listing pages.

--- next/components/ListAllHouses.tsx
+++ next/components/ListAllHouses.tsx
@@ -1,4 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
+import { BoxLayoutSingle } from '../@/components/component/box-layout-single'
+import { Container } from '../@/components/component/container'
+import { HouseListing } from '../@/components/component/house-listing'
 
 export default function ListAllHouses() {
   const { isLoading, error, data } = useQuery({
@@ -18,15 +21,10 @@ export default function ListAllHouses() {
   if (error) return <div>Failed to load</div>
 
   return (
-    <div>
-      <h2 className="text-2xl leading-snug my-4 mx-0">Houses</h2>
-      <ul className="list-none p-0 m-0">
-        {data.map(({ house_id, house_address, house_rooms, house_storage, house_food, house_wood }) => (
-          <li className="mt-0 mx-0 mb-5" key={house_id}>
-            <p>The {house_address.house_address_number + " " + house_address.house_address_road.house_road_name} house has {house_rooms} rooms and {house_storage} storage. It has {house_food.resource_volume} food and {house_wood.resource_volume} wood in storage.</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <BoxLayoutSingle>
+      <Container>
+        <HouseListing houseData={data} />
+      </Container>
+    </BoxLayoutSingle>
   )
 }
--- next/components/ListAllPeople.tsx
+++ next/components/ListAllPeople.tsx
@@ -1,4 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
+import { BoxLayoutSingle } from '../@/components/component/box-layout-single'
+import { Container } from '../@/components/component/container'
+import { PersonListing } from '../@/components/component/person-listing'
 
 export default function ListAllPeople() {
   const { isLoading, error, data } = useQuery({
@@ -20,17 +23,10 @@ export default function ListAllPeople() {
   console.log(data)
 
   return (
-    <div>
-      <h2 className="text-2xl leading-snug my-4 mx-0">People</h2>
-      <ul className="list-none p-0 m-0">
-        {
-          data.map(({ person_id, person_name, person_family, person_age }) => (
-            <li className="mt-0 mx-0 mb-5" key={person_id}>
-              <p>{person_name} {person_family.family_name} is {person_age} years old.</p>
-            </li>
-          ))
-        }
-      </ul>
-    </div>
+    <BoxLayoutSingle>
+      <Container>
+        <PersonListing personData={data} />
+      </Container>
+    </BoxLayoutSingle>
   )
 }
