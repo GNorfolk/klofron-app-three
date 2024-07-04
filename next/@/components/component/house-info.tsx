ba940730b6cba3c70dd180524e038d06cdd5e85e
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-07-04 01:47:19 +0100

    Making house characteristics use dynamic grid layout.

--- next/@/components/component/house-info.tsx
+++ next/@/components/component/house-info.tsx
@@ -3,42 +3,38 @@ import { UsersIcon, HandPlatterIcon, TreesIcon, BuildingIcon, BoxIcon, BedIcon, 
 export function HouseInfo({ houseData }) {
   return (
     <main>
-      <div className="flex">
+      <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-2 2xl:grid-cols-4">
         <div className="flex items-center mt-4 mr-4 text-sm text-gray-500 dark:text-gray-400">
           <BedIcon className="w-5 h-5 mr-2" />
-          <span>{houseData.house_rooms} Bed</span>
+          <span className='whitespace-nowrap'>{houseData.house_rooms} Bed</span>
         </div>
         <div className="flex items-center mt-4 mr-4 text-sm text-gray-500 dark:text-gray-400">
           <UsersIcon className="w-5 h-5 mr-2" />
-          <span>{houseData.house_people.length} People</span>
+          <span className='whitespace-nowrap'>{houseData.house_people.length} People</span>
         </div>
         <div className="flex items-center mt-4 mr-4 text-sm text-gray-500 dark:text-gray-400">
           <BuildingIcon className="w-5 h-5 mr-2" />
-          <span>{houseData.house_rooms - houseData.house_people.length} Room</span>
+          <span className='whitespace-nowrap'>{houseData.house_rooms - houseData.house_people.length} Room</span>
         </div>
-      </div>
-      <div className="flex">
         <div className="flex items-center mt-4 mr-4 text-sm text-gray-500 dark:text-gray-400">
           <HandPlatterIcon className="w-5 h-5 mr-2" />
-          <span>{houseData.house_food.resource_volume} Food</span>
+          <span className='whitespace-nowrap'>{houseData.house_food.resource_volume} Food</span>
         </div>
         <div className="flex items-center mt-4 mr-4 text-sm text-gray-500 dark:text-gray-400">
           <TreesIcon className="w-5 h-5 mr-2" />
-          <span>{houseData.house_wood.resource_volume} Wood</span>
+          <span className='whitespace-nowrap'>{houseData.house_wood.resource_volume} Wood</span>
         </div>
         <div className="flex items-center mt-4 mr-4 text-sm text-gray-500 dark:text-gray-400">
           <TruckIcon className="w-5 h-5 mr-2" />
-          <span>0 Trade</span>
+          <span className='whitespace-nowrap'>0 Trade</span>
         </div>
-      </div>
-      <div className="flex">
         <div className="flex items-center mt-4 mr-4 text-sm text-gray-500 dark:text-gray-400">
           <WarehouseIcon className="w-5 h-5 mr-2" />
-          <span>{houseData.house_storage} Storage</span>
+          <span className='whitespace-nowrap'>{houseData.house_storage} Storage</span>
         </div>
         <div className="flex items-center mt-4 mr-4 text-sm text-gray-500 dark:text-gray-400">
           <BoxIcon className="w-5 h-5 mr-2" />
-          <span>{houseData.house_storage - houseData.house_food.resource_volume - houseData.house_wood.resource_volume} Capacity</span>
+          <span className='whitespace-nowrap'>{houseData.house_storage - houseData.house_food.resource_volume - houseData.house_wood.resource_volume} Capacity</span>
         </div>
       </div>
     </main>
