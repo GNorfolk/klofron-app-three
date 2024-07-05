464f30d5a15c9840930ae1497e957a71dfc21703
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-07-05 01:42:59 +0100

    Adding findAll and findOne service functions.

--- nest/src/betrothal/betrothal.service.ts
+++ nest/src/betrothal/betrothal.service.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBetrothalDto } from './dto/create-betrothal.dto';
 import { UpdateBetrothalDto } from './dto/update-betrothal.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Betrothal } from './entities/Betrothal';
+import { Repository, DataSource } from 'typeorm';
 
 @Injectable()
 export class BetrothalService {
+  constructor(
+    @InjectRepository(Betrothal) private betrothalRepository: Repository<Betrothal>,
+    private dataSource: DataSource
+  ) {}
+
   async create(createBetrothalDto: CreateBetrothalDto) {
     return 'This action adds a new betrothal';
   }
 
-  async findAll() {
-    return [`This action returns all betrothal`];
+  async findAll(): Promise<Betrothal[]> {
+    let betrothals = this.betrothalRepository
+      .createQueryBuilder("betrothal")
+    return await betrothals.getMany();
   }
 
-  async findOne(id: number) {
-    return `This action returns a #${id} betrothal`;
+  async findOne(id: number): Promise<Betrothal> {
+    const betrothal = this.betrothalRepository
+      .createQueryBuilder("betrothal")
+      .where("betrothal.betrothal_id = :id", { id: id })
+    return await betrothal.getOne();
   }
 
   async update(id: number, updateBetrothalDto: UpdateBetrothalDto) {
