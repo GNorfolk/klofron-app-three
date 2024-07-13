11f64f1a5f0d5893bc511e92dd938065210ecb05
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-07-13 23:09:41 +0100

    Adding betrothal page and component and betrothal-listing component, updating family to return betrothal data.

---
+++ next/@/components/component/betrothal-listing.tsx
@@ -0,0 +1,43 @@
+import { ChurchIcon } from '../ui/icon'
+
+export function BetrothalListing({ familyPeople, accepterId = null }) {
+  const familyPeopleBetrothals = familyPeople.filter(ppl => ppl.person_betrothal_receipts.length > 0)
+  return (
+    <main>
+      <h2 className="text-2xl leading-snug my-4 mx-0">Betrothal Info</h2>
+      { familyPeopleBetrothals.length > 0 ? familyPeopleBetrothals.map(({ person_id, person_name, person_betrothal_receipts }) => (
+        <a href={"/person/" + person_id + "/proposal"} className="p-6 pt-2 pb-2">
+          <div className="flex">
+            <div className="flex items-center text-sm text-gray-500 dark:text-gray-400">
+              <ChurchIcon className="w-5 h-5 min-w-5 min-h-5 mx-2" />
+              <span className='whitespace-nowrap'>{person_name} has {person_betrothal_receipts.length} betrothal.</span>
+            </div>
+          </div>
+        </a>
+      )) :
+      <p className="m-2 text-gray-500 dark:text-gray-400">No family members have betrothals!</p>
+      }
+    </main>
+  )
+}
+
+export function BetrothalEligibleListing({ familyPeople, accepterId = null }) {
+  const familyPeopleBachelors = familyPeople.filter(ppl => ppl.person_age >= 18 && ppl.person_partner_id === null)
+  return (
+    <main>
+      <h2 className="text-2xl leading-snug my-4 mx-0">Betrothal Eligible Info</h2>
+      { familyPeopleBachelors.length > 0 ? familyPeopleBachelors.map(({ person_id, person_name, person_betrothal_receipts }) => (
+        <a href={"/person/" + person_id + "/proposal"} className="p-6 pt-2 pb-2">
+          <div className="flex">
+            <div className="flex items-center text-sm text-gray-500 dark:text-gray-400">
+              <ChurchIcon className="w-5 h-5 min-w-5 min-h-5 mx-2" />
+              <span className='whitespace-nowrap'>{person_name} is eligible for betrothal.</span>
+            </div>
+          </div>
+        </a>
+      )) :
+      <p className="m-2 text-gray-500 dark:text-gray-400">No family members are eligible for betrothal!</p>
+      }
+    </main>
+  )
+}
---
+++ next/components/DescribeFamilyBetrothals.tsx
@@ -0,0 +1,43 @@
+import { useRouter } from 'next/router'
+import { useQuery } from '@tanstack/react-query'
+import { useForm, SubmitHandler } from "react-hook-form"
+import axios from 'axios'
+import { BoxLayoutSingle } from '../@/components/component/box-layout-single'
+import { Container } from '../@/components/component/container'
+import { BetrothalListing, BetrothalEligibleListing } from '../@/components/component/betrothal-listing'
+
+export default function DescribeFamilyBetrothals({ queryClient, userId }) {
+  const router = useRouter()
+  if (router.isReady) {
+    const { isLoading, error, data } = useQuery({
+      queryKey: ['familyProposalData' + router.query.id],
+      queryFn: () =>
+        fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/family/' + router.query.id).then(
+          (res) => res.json(),
+        ),
+    })
+    
+    if (isLoading) return (
+      <div>
+        <h2 className="text-2xl leading-snug my-4 mx-0">Proposal Info</h2>
+        <p>Loading...</p>
+      </div>
+    )
+    if (error) return <div>Failed to load</div>
+
+    if (data.family_user_id === userId) {
+      return (
+        <BoxLayoutSingle>
+          <Container>
+            <BetrothalListing familyPeople={data.family_people} />
+          </Container>
+          <Container>
+            <BetrothalEligibleListing familyPeople={data.family_people} />
+          </Container>
+        </BoxLayoutSingle>
+      )
+    } else {
+      router.push('/')
+    }
+  }
+}
