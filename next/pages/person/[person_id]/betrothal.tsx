40ac3b6b6f48a543e09a3f7eddd708043453043b
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-07-14 23:16:42 +0100

    Creating basic person betrothal page.

---
+++ next/pages/person/[person_id]/betrothal.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BaseLayout } from '../../../@/components/component/base-layout'
+import DescribePersonBetrothal from '../../../components/DescribePersonBetrothal'
+import { useSession } from 'next-auth/react'
+
+export default function Family({ client }) {
+  const { status, data } = useSession()
+  const userId = data?.user ? data.user.id : null
+  return (
+    <BaseLayout>
+      <QueryClientProvider client={client}>
+        <DescribePersonBetrothal userId={userId} queryClient={client} />
+      </QueryClientProvider>
+      <div className="mt-12 mx-0 mb-0">
+        <Link href="/">← Back to home</Link>
+      </div>
+    </BaseLayout>
+  )
+}
