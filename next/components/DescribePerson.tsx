300d72e8653172994e75591cc7df6ea4c4c8b361
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-07-24 10:31:52 +0100

    Cleaning up DescribePerson component too.

--- next/components/DescribePerson.tsx
+++ next/components/DescribePerson.tsx
@@ -34,7 +34,6 @@ export default function DescribePerson({ queryClient, status, userId = null }) {
               status === "authenticated" && userId == data.person_family.family_user_id ?
               <div>
                 <ListPersonBetrothals data={data} />
-                {/* <ListPersonProposals data={data} userId={userId} showOffers={false} queryClient={queryClient} /> */}
               </div>
               :
               <></>
@@ -138,131 +137,6 @@ function ListPersonActionsPrevious({ personId }) {
               <p>This person has no past actions initiated.</p>
             </li>
         </ul>
-      </Container>
-    )
-  }
-}
-
-function ListPersonProposals({ data, showLink = true, queryClient = null, userId, showOffers = true }) {
-  const router = useRouter()
-  if (router.isReady) {
-    const createProposal = useMutation({
-      mutationFn: (id) => {
-        return axios.post(process.env.NEXT_PUBLIC_API_HOST + '/v2/proposal', {
-          proposal_person_id: id
-        })
-      },
-    })
-
-    type Inputs = {
-      proposal_offer_id: number
-      accepter_person_id: number
-    }
-
-    const {
-      register,
-      handleSubmit,
-      watch,
-      formState: { errors },
-    } = useForm<Inputs>()
-
-    const onSubmit: SubmitHandler<Inputs> = (formData) => {
-      axios.patch(process.env.NEXT_PUBLIC_API_HOST + '/v2/proposal-offer/' + formData.proposal_offer_id, {
-        accepter_person_id: formData.accepter_person_id,
-      }).then(response => {
-        queryClient.invalidateQueries()
-        document.getElementById("cm-" + userId).innerText = ' '
-      }).catch(error => {
-        document.getElementById("cm-" + userId).innerText = error.response.data.message
-      })
-    }
-
-    const familyBachelors = data.person_family.family_people.filter(ppl =>
-      ppl.person_age >= 18 && ppl.person_partner_id === null
-    )
-
-    return (
-      <Container>
-        <h2 className="text-2xl leading-snug my-4 mx-0">Betrothal Info</h2>
-        <ul className="list-none p-0 m-0">
-          {
-            data.person_proposals.length > 0 ?
-              data.person_proposals.map(({ proposal_id, proposal_offers }) => (
-                <li className="mt-0 mx-0 mb-5" key={data.proposal_id}>
-                  <p>{data.person_name} has an existing proposal with id {proposal_id}.</p>
-                  {
-                    showOffers ?
-                      proposal_offers.length > 0 ?
-                        proposal_offers.map(({ proposal_offer_person, proposal_offer_dowry }) => (
-                          <p>
-                            Person with proposal is {data.person_name} who is {data.person_gender} and is {data.person_age} years old.
-                            Person {data.person_name} will be betrothed to {proposal_offer_person.person_name} who is {proposal_offer_person.person_gender} and {proposal_offer_person.person_age} years old. 
-                            They are also offering {proposal_offer_dowry.proposal_dowry_person.person_name} who is {proposal_offer_dowry.proposal_dowry_person.person_gender} and {proposal_offer_dowry.proposal_dowry_person.person_age} years old.
-                          </p>
-                        ))
-                      :
-                        <p>This proposal has no proposal offers.</p>
-                    :
-                      null
-                  }
-                  {
-                    proposal_offers.length > 0 ?
-                      familyBachelors.length > 0 ?
-                        <div>
-                          <form onSubmit={handleSubmit(onSubmit)}>
-                            <select {...register("proposal_offer_id", { required: true })}>
-                              {
-                                errors.proposal_offer_id ? document.getElementById("cm-" + router.query.id).innerText = "The Person field is required" : null
-                              }
-                              {
-                                proposal_offers.map(({ proposal_offer_id, proposal_offer_person, proposal_offer_dowry }) => (
-                                  <option value={proposal_offer_id}>{proposal_offer_person.person_name + " / " + proposal_offer_dowry.proposal_dowry_person.person_name}</option>
-                                ))
-                              }
-                            </select>
-                            <select {...register("accepter_person_id", { required: true })}>
-                              {
-                                errors.accepter_person_id ? document.getElementById("cm-" + router.query.id).innerText = "The Person field is required" : null
-                              }
-                              {
-                                familyBachelors.map(({ person_id, person_name }) => (
-                                  <option value={person_id}>{person_name}</option>
-                                ))
-                              }
-                            </select>
-                            <input type="submit" />
-                          </form>
-                          <small className="text-stone-500" id={'cm-' + userId}></small>
-                        </div>
-                      :
-                        <p>There are no family people eligible to accept an offer.</p>
-                    :
-                      null
-                  }
-                </li>
-              ))
-            :
-              <div>
-                <p>{data.person_name} is not open to proposal offers!</p>
-                {
-                  data.person_family.family_user_id === userId ?
-                    <button onClick={
-                      () => {
-                          createProposal.mutate(data.person_id, { onSettled: (res) => {
-                            queryClient.invalidateQueries()
-                          }
-                        })
-                      }
-                    }>Create Proposal</button>
-                  :
-                    null
-                }
-              </div>
-          }
-        </ul>
-        {
-          showLink ? <p>Go to <Link href={`/person/${router.query.person_id}/proposal`}>Proposals</Link> page.</p> : null
-        }
       </Container>
     )
   }
