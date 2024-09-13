dc58ddbc63fdb2271de5aa3022b787b1f561ae8e
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-09-14 00:46:07 +0100

    Setting up package.json to deploy via open-next builder.

--- next/tf/deploy/main.tf
+++ next/tf/deploy/main.tf
@@ -1,13 +1,13 @@
 module "this" {
   source  = "nhs-england-tools/opennext/aws"
-  version = "1.0.6" # Use the latest release from https://github.com/nhs-england-tools/terraform-aws-opennext/releases
+  version = "1.0.0" # Use the latest release from https://github.com/nhs-england-tools/terraform-aws-opennext/releases
 
   prefix              = "ka3"                               # Prefix for all created resources
-  opennext_build_path = "../.open-next"                     # Path to your .open-next folder
+  opennext_build_path = "../../.open-next"                  # Path to your .open-next folder
   hosted_zone_id      = data.aws_route53_zone.this.zone_id  # The Route53 hosted zone ID for your domain name
 
   cloudfront = {
     aliases             = ["www.klofron.uk"]                        # Your domain name
-    acm_certificate_arn = aws_acm_certificate.this.certificate_arn  # The ACM (SSL) certificate for your domain
+    acm_certificate_arn = data.aws_acm_certificate.this.arn         # The ACM (SSL) certificate for your domain
   }
 }
