baca81ee35222b08eec0ee6a9f491d6199623510
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-09-19 23:34:02 +0100

    Fixing bug with StyledSelect useFormContext returning null.

--- next/@/components/ui/input.tsx
+++ next/@/components/ui/input.tsx
@@ -25,9 +25,9 @@ Input.displayName = "Input"
 export { Input }
 
 export function StyledSelect({ children, fieldName, fieldRequired, ...props } : { children: React.ReactNode, fieldName: string, fieldRequired: boolean }) {
-  const { register } = useFormContext();
+  const methods = useFormContext();
   return (
-    <select {...register(fieldName, { required: fieldRequired })} className="flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1" {...props}>
+    <select {...methods?.register(fieldName, { required: fieldRequired })} className="flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1" {...props}>
       {children}
     </select>
   )
