2d58b7fdaa072aeeff24d07d53725c4b0c1ea4fb
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-09-28 23:44:24 +0100

    Moving CreateBetrothal component into pages.

--- next/pages/family/[family_id]/betrothal/[person_id].tsx
+++ next/pages/family/[family_id]/betrothal/[person_id].tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useQuery, QueryClientProvider } from '@tanstack/react-query'
 import { BaseLayout } from '../../../../@/components/component/base-layout'
-import CreateBetrothal from '../../../../components/CreateBetrothal'
 import { useSession } from 'next-auth/react'
+import { BoxLayout } from '../../../../@/components/component/box-layout'
+import { Container } from '../../../../@/components/component/container'
+import { BetrothalCreation } from '../../../../@/components/component/betrothal'
+import { PersonInfo } from '../../../../@/components/component/person'
+import { HeaderTwo } from '../../../../@/components/ui/header'
 
-export default function Family({ client, router }) {
+export default function Main({ client, router }) {
   const { status, data } = useSession()
   const userId = data?.user ? data.user.id : null
   return (
@@ -16,5 +20,67 @@ export default function Family({ client, router }) {
       <Link href="/">← Back to home</Link>
     </div>
   </BaseLayout>
+  )
+}
+
+export function CreateBetrothal({ queryClient, userId, router }) {
+  if (router.isReady) {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <BoxLayout left={
+          <CreateBetrothalInfo personId={router.query.person_id} />
+        } right={
+          <CreateBetrothalForm familyId={router.query.family_id} personId={router.query.person_id} queryClient={queryClient} />
+        } />
+      </QueryClientProvider>
+    )
+  }
+}
+
+export function CreateBetrothalInfo({ personId }) {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['CreateBetrothalInfoData' + personId],
+    queryFn: () =>
+      fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/person/' + personId).then(
+        (res) => res.json(),
+      ),
+  })
+
+  if (isLoading) return (
+    <div>
+      <HeaderTwo>Betrothal Info</HeaderTwo>
+      <p>Loading...</p>
+    </div>
+  )
+  if (error) return <div>Failed to load</div>
+
+  return (
+    <Container>
+      <PersonInfo title="Betrothal" personInfo={data} />
+    </Container>
+  )
+}
+
+export function CreateBetrothalForm({ familyId, personId, queryClient }) {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['CreateBetrothalFormData' + familyId],
+    queryFn: () =>
+      fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/family/' + familyId).then(
+        (res) => res.json(),
+      ),
+  })
+
+  if (isLoading) return (
+    <div>
+      <HeaderTwo>Betrothal Info</HeaderTwo>
+      <p>Loading...</p>
+    </div>
+  )
+  if (error) return <div>Failed to load</div>
+
+  return (
+    <Container>
+      <BetrothalCreation peopleData={data.family_people} familyId={familyId} personId={personId} queryClient={queryClient} familyName={data.family_name} />
+    </Container>
   )
 }
