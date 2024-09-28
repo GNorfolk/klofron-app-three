2dbdc454fdbe51c92d61023ad18c632671e191c4
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-09-29 00:09:13 +0100

    Moving ListAllEntities component into pages.

--- next/pages/directory/index.tsx
+++ next/pages/directory/index.tsx
@@ -1,9 +1,14 @@
 import Link from 'next/link'
 import { BaseLayout } from '../../@/components/component/base-layout'
-import { QueryClientProvider } from '@tanstack/react-query'
-import ListAllEntities from '../../components/ListAllEntities'
+import { QueryClientProvider, useQuery } from '@tanstack/react-query'
+import { BoxLayout } from '../../@/components/component/box-layout'
+import { Container } from '../../@/components/component/container'
+import { FamilyListing } from '../../@/components/component/family'
+import { PersonListing } from '../../@/components/component/person'
+import { HouseListing } from '../../@/components/component/house'
+import { HeaderOne, HeaderTwo } from '../../@/components/ui/header'
 
-export default function Family({ client, router }) {
+export default function Main({ client, router }) {
   return (
     <BaseLayout>
       <QueryClientProvider client={client}>
@@ -13,5 +18,96 @@ export default function Family({ client, router }) {
         <Link href="/">← Back to home</Link>
       </div>
     </BaseLayout>
+  )
+}
+
+export function ListAllEntities({ queryClient }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BoxLayout left={
+        <div>
+          <ListAllFamilies />
+          <ListAllHouses />
+        </div>
+      } right={
+        <ListAllPeople />
+      }/>
+    </QueryClientProvider>
+  )
+}
+
+function ListAllFamilies({ queryClient = null, userId = null }) {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['familiesData'],
+    queryFn: () =>
+      fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/family').then(
+        (res) => res.json(),
+      ),
+  })
+
+  if (isLoading) return (
+    <div>
+      <Container>
+        <HeaderOne>Families</HeaderOne>
+        <p>Loading...</p>
+      </Container>
+    </div>
+  )
+  if (error) return <div>Failed to load</div>
+
+  return (
+    <Container>
+      <FamilyListing familyData={data} />
+    </Container>
+  )
+}
+
+function ListAllHouses() {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['housesData'],
+    queryFn: () =>
+      fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/house').then(
+        (res) => res.json(),
+      ),
+  })
+
+  if (isLoading) return (
+    <div>
+      <HeaderTwo>Houses</HeaderTwo>
+      <p>Loading...</p>
+    </div>
+  )
+  if (error) return <div>Failed to load</div>
+
+  return (
+    <Container>
+      <HouseListing houseData={data} />
+    </Container>
+  )
+}
+
+function ListAllPeople() {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['listAllPeopleData'],
+    queryFn: () =>
+      fetch(process.env.NEXT_PUBLIC_API_HOST + '/v2/person').then(
+        (res) => res.json(),
+      ),
+  })
+
+  if (isLoading) return (
+    <div>
+      <HeaderTwo>People</HeaderTwo>
+      <p>Loading...</p>
+    </div>
+  )
+  if (error) return <div>Failed to load</div>
+
+  console.log(data)
+
+  return (
+    <Container>
+      <PersonListing personData={data} />
+    </Container>
   )
 }
