1ec9fc1dc944b3df453f3068842a2241358523c7
George Norfolk <georgenorfolk@hotmail.co.uk>
2024-09-29 23:17:34 +0100

    Fixing routing on index page, and on base layout.

--- next/components/component/base-layout.tsx
+++ next/components/component/base-layout.tsx
@@ -20,18 +20,18 @@ export function BaseLayout({ children } : { children: React.ReactNode }) {
             <ul className="grid gap-1">
               <li className="flex items-center gap-3 px-6 py-2 text-gray-500 hover:bg-gray-900 hover:text-gray-400 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-50">
                 <Link href="/">
-                  <section className="flex">
+                  <a className="flex">
                     <HomeIcon className="w-5 h-5 min-w-5 min-h-5" />
                     <span className='ml-2'>Home</span>
-                  </section>
+                  </a>
                 </Link>
               </li>
               <li className="flex items-center gap-3 px-6 py-2 text-gray-500 hover:bg-gray-900 hover:text-gray-400 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-50">
                 <Link href="/directory">
-                  <section className="flex">
+                  <a className="flex">
                     <MicroscopeIcon className="w-5 h-5 min-w-5 min-h-5" />
                     <span className='ml-2'>Directory</span>
-                  </section>
+                  </a>
                 </Link>
               </li>
               { status === "authenticated" ? (
@@ -55,10 +55,12 @@ export function BaseLayout({ children } : { children: React.ReactNode }) {
       </div>
       <div className="flex flex-col">
         <header className="flex h-16 items-center justify-between border-b border-gray-800 bg-black px-4 dark:border-gray-700 dark:bg-gray-950 md:hidden">
-          <a href="/" className="flex items-center gap-2 font-semibold">
-            <MountainIcon className="h-6 w-6" />
-            <span className="text-gray-500 dark:text-gray-50">klofroN</span>
-          </a>
+          <Link href="/">
+            <a className="flex items-center gap-2 font-semibold">
+              <MountainIcon className="h-6 w-6" />
+              <span className="text-gray-500 dark:text-gray-50">klofroN</span>
+            </a>
+          </Link>
           <Sheet>
             <SheetTrigger asChild>
               <Button size="icon" variant="ghost">
@@ -77,18 +79,18 @@ export function BaseLayout({ children } : { children: React.ReactNode }) {
                 <ul className="grid gap-1">
                   <li className="flex items-center gap-3 px-6 py-2 text-gray-500 hover:bg-gray-900 hover:text-gray-400 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-50">
                     <Link href="/">
-                      <section className="flex">
+                      <a className="flex">
                         <HomeIcon className="w-5 h-5 min-w-5 min-h-5" />
                         <span className='ml-2'>Home</span>
-                      </section>
+                      </a>
                     </Link>
                   </li>
                   <li className="flex items-center gap-3 px-6 py-2 text-gray-500 hover:bg-gray-900 hover:text-gray-400 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-50">
                     <Link href="/directory">
-                      <section className="flex">
+                      <a className="flex">
                         <MicroscopeIcon className="w-5 h-5 min-w-5 min-h-5" />
                         <span className='ml-2'>Directory</span>
-                      </section>
+                      </a>
                     </Link>
                   </li>
                   { status === "authenticated" ? (
--- next/components/component/family.tsx
+++ next/components/component/family.tsx
@@ -8,19 +8,21 @@ export function FamilyListing({ familyData }) {
     <main>
       <HeaderOne>Families</HeaderOne>
       { familyData.length > 0 ? familyData.map(({ family_id, family_name, family_people, family_houses }) => (
-        <a href={`/family/${family_id}`} className="p-6 pt-2 pb-2">
-          <HeaderThree>{"The " + family_name + " family"}</HeaderThree>
-          <div className="grid grid-cols-2">
-            <DivIconInfo>
-              <UsersIcon className="w-5 h-5 min-w-5 min-h-5 mr-2" />
-              <span className='whitespace-nowrap'>{family_people.length} people</span>
-            </DivIconInfo>
-            <DivIconInfo>
-              <HomeIcon className="w-5 h-5 min-w-5 min-h-5 mr-2" />
-              <span className='whitespace-nowrap'>{family_houses.length} houses</span>
-            </DivIconInfo>
-          </div>
-        </a>
+        <Link href={`/family/${family_id}`}>
+          <a className="p-6 pt-2 pb-2">
+            <HeaderThree>{"The " + family_name + " family"}</HeaderThree>
+            <div className="grid grid-cols-2">
+              <DivIconInfo>
+                <UsersIcon className="w-5 h-5 min-w-5 min-h-5 mr-2" />
+                <span className='whitespace-nowrap'>{family_people.length} people</span>
+              </DivIconInfo>
+              <DivIconInfo>
+                <HomeIcon className="w-5 h-5 min-w-5 min-h-5 mr-2" />
+                <span className='whitespace-nowrap'>{family_houses.length} houses</span>
+              </DivIconInfo>
+            </div>
+          </a>
+        </Link>
       )) :
         <a className="p-6 pt-2 pb-2">
           <HeaderThree>The user has no families!</HeaderThree>
