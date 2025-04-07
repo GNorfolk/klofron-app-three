b747cf21ab8fd8998653c0db23c61c6f3fc115a9
George Norfolk <georgenorfolk@hotmail.co.uk>
2025-04-07 14:17:18 +0100

    Fixing nextjs rewrites for backend on local, adding proposal module to replace betrothals.

--- next/next.config.js
+++ next/next.config.js
@@ -28,12 +28,8 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/v1/:path*',
-        destination: 'http://localhost:3001/v1/:path*'
-      },
-      {
-        source: '/v2/:path*',
-        destination: 'http://localhost:5000/v2/:path*'
+        source: '/v:version/:path*',
+        destination: 'http://localhost:5000/v:version/:path*'
       }
     ]
   }
