76ada092fe2d6d528f71a13cda39fac8ff54e76c
George Norfolk <georgenorfolk@hotmail.co.uk>
2025-04-12 00:18:55 +0100

    Setting up ListAllPeople to pull data via getServerSideProps.

--- next/pages/test/index.tsx
+++ next/pages/test/index.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { BaseLayout } from '@/components/component/base-layout'
 import { QueryClientProvider, useQuery, QueryClient } from '@tanstack/react-query'
-import { BoxLayoutSingle } from '@/components/component/box-layout'
+import { BoxLayout } from '@/components/component/box-layout'
 import { Container } from '@/components/component/container'
 import { FamilyListing } from '@/components/component/family'
 import { PersonListing } from '@/components/component/person'
@@ -11,14 +11,18 @@ import { Paragraph } from '@/components/ui/text'
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 
 
-export default function Main({ client, router, familiesData, housesData }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Main({ client, router, familiesData, housesData, peopleData }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <BaseLayout>
       <QueryClientProvider client={client}>
-        <BoxLayoutSingle>
-          <ListAllFamilies familiesData={familiesData} />
-          <ListAllHouses housesData={housesData} />
-        </BoxLayoutSingle>
+        <BoxLayout left={
+          <div>
+            <ListAllFamilies familiesData={familiesData} />
+            <ListAllHouses housesData={housesData} />
+          </div>
+        } right={
+          <ListAllPeople peopleData={peopleData} />
+        }/>
       </QueryClientProvider>
       <div className="mt-12 mx-0 mb-0">
         <Link href="/">← Back to home</Link>
@@ -59,18 +63,38 @@ function ListAllHouses({ housesData }) {
   )
 }
 
+function ListAllPeople({ peopleData }) {
+  if (!peopleData)
+    return (
+      <Container>
+        <HeaderOne>People</HeaderOne>
+        <Paragraph>Failed to load!</Paragraph>
+      </Container>
+    )
+
+  return (
+    <Container>
+      <PersonListing personData={peopleData} />
+    </Container>
+  )
+}
+
+
 export const getServerSideProps: GetServerSideProps = async () => {
   try {
     const resFamily = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/v2/family`)
     const resHouse = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/v2/house`)
+    const resPerson = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/v2/person`)
 
     const familiesData = await resFamily.json()
     const housesData = await resHouse.json()
+    const peopleData = await resPerson.json()
 
     return {
       props: {
         familiesData,
-        housesData
+        housesData,
+        peopleData,
       },
     }
   } catch (error) {
@@ -78,7 +102,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
     return {
       props: {
         familiesData: null,
-        housesData: null
+        housesData: null,
+        peopleData: null,
       },
     }
   }
