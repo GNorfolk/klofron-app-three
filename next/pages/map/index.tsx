118cf334c5a6c764c7580e837a1c780d7b769be9
George Norfolk <georgenorfolk@hotmail.co.uk>
2025-04-28 23:45:58 +0100

    Setting up frontend to pull data from backend and show entire hex grid sourced from mysql.

--- next/pages/map/index.tsx
+++ next/pages/map/index.tsx
@@ -20,20 +20,40 @@ export default function Main({ client, router }) {
 }
 
 export function ShowHexMap({}) {
-  const fill = ['fill-blue-300', 'fill-green-800']
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['mapData'],
+    queryFn: () =>
+      fetch(process.env.NEXT_PUBLIC_API_HOST + '/v1/hex').then(
+        (res) => res.json(),
+      ),
+  })
+
+  if (isLoading) return (
+    <div>
+      <p>loading...</p>
+    </div>
+  )
+  if (error) return (
+    <div>
+      <p>oh dear</p>
+    </div>
+  )
+  
   return (
     <div>
       <HexGrid width={1200} height={500} viewBox="-50 -50 100 100">
         <Layout size={{ x: 5, y: 5 }} flat={false} spacing={1.08} origin={{ x: 0, y: 0 }}>
-          <Hexagon q={0} r={0} s={0} className={fill[1] + ' stroke-slate-500 stroke-[0.2]'} />
-          <Hexagon q={0} r={-1} s={1} className={fill[1] + ' stroke-slate-500 stroke-[0.2]'} />
-          <Hexagon q={0} r={1} s={-1} className={fill[0] + ' stroke-slate-500 stroke-[0.2]'} />
-          <Hexagon q={1} r={-1} s={0} className={fill[1] + ' stroke-slate-500 stroke-[0.2]'} />
-          <Hexagon q={1} r={0} s={-1} className={fill[1] + ' stroke-slate-500 stroke-[0.2]'} />
-          <Hexagon q={-1} r={1} s={0} className={fill[0] + ' stroke-slate-500 stroke-[0.2]'} />
-          <Hexagon q={-1} r={0} s={1} className={fill[1] + ' stroke-slate-500 stroke-[0.2]'} />
+          {
+            data.map(({ hex_id, hex_q_coordinate, hex_r_coordinate, hex_s_coordinate, hex_land }) => (
+              <Hexagon key={hex_id} q={hex_q_coordinate} r={hex_r_coordinate} s={hex_s_coordinate} className={getColour(hex_land) + ' stroke-slate-500 stroke-[0.2]'} />
+            ))
+          }
         </Layout>
       </HexGrid>
     </div>
   );
+}
+
+function getColour(isLand: boolean) {
+  return isLand ? 'fill-green-800' : 'fill-blue-800'
 }
